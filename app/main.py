7ebc63a1d8509936d7b865843ee82c0f3111ba02
Roman <romanschneider9@gmail.com>
2022-05-10 15:52:16 +0300

    python -m venv venv

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
