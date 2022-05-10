51a1af3f4c434bce00f0697e79a39c8253ac4edc
Олег Белый <belochard@gmail.com>
2022-05-10 18:03:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -17,5 +17,7 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # write your code here
-sorted_dictionary = {"Mutable": [my_favourite_films, profile_info, marks, collection_of_coins], 
-                           "Immutable": [lucky_number, one_is_a_prime_number, name]}
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, profile_info, one_is_a_prime_number, pi, name]
+}
