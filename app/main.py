29be748f84d1bc8344056e22a564eb4acfad1485
Yura Beseda <yuriy.beseda@gmail.com>
2022-05-11 08:27:06 +0200

    'Deleted unnecessary comment'

--- app/main.py
+++ app/main.py
@@ -16,7 +16,6 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
 sorted_variables = {
     "mutable": [my_favourite_films, collection_of_coins, marks],
     "immutable": [one_is_a_prime_number, lucky_number, pi, profile_info, name]
