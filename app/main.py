907e73c3b93e4040ec946adf284595d6f2bebbc8
Vladimir Titarenko <tytarenkova@gmail.com>
2022-09-12 12:31:55 +0300

    The indentation is fixed

--- app/main.py
+++ app/main.py
@@ -16,9 +16,8 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-
 sorted_variables = \
     {'mutable':
         [collection_of_coins, marks, my_favourite_films],
      'immutable':
-         [lucky_number, name, one_is_a_prime_number, pi, profile_info]}
+        [lucky_number, name, one_is_a_prime_number, pi, profile_info]}
