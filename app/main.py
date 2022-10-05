d8f3b15552f75c3b298cf26369b979726a47eac0
Admin <winofkill@gmail.com>
2022-10-05 13:48:06 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,20 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+sorted_variables = {}
+
+def sorted(*args) -> dict:
+
+    for i in args:
+        group = "immutable"
+        if type(i) == set or type(i) == dict or type(i) == list:
+            group = "mutable"
+
+        if group not in sorted_variables:
+            sorted_variables[group] = [i]
+        else:
+            sorted_variables[group].append(i)
+    return sorted_variables
+
+print(sorted(pi,lucky_number,collection_of_coins,marks,profile_info,my_favourite_films,name,one_is_a_prime_number))
