5578a4782aebc289bb53a66e52596f4d9c5d806d
Vitalii Panochko <vpapplic@gmail.com>
2022-11-15 13:29:48 +0200

    Redone sorted variables

--- app/main.py
+++ app/main.py
@@ -16,32 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-variable_mass = \
-    [
-        lucky_number,
-        pi,
-        one_is_a_prime_number,
-        name,
-        my_favourite_films,
-        profile_info,
-        marks,
-        collection_of_coins,
-    ]
-
-
-def sorted_variables(mass: list) -> dict:
-    result = {
-        "mutable": [],
-        "immutable": [],
-    }
-
-    for arg in mass:
-        if isinstance(arg, (list, set, dict)):
-            result["mutable"].append(arg)
-        if isinstance(arg, (int, float, bool, str, tuple)):
-            result["immutable"].append(arg)
-
-    return result
-
-
-sorted_variables = sorted_variables(variable_mass)
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
+}
