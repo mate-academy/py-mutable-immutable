3d149473b599f7953428ab9730a36c73ab9a02da
Volodymyr Derevyanko <derev00054@gmail.com>
2022-11-19 03:09:24 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,27 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+
+
+def kor(*args) -> dict:
+
+    if type(*args) == list or type(*args) == dict or type(*args) == set:
+        sorted_variables["mutable"].append(*args)
+    else:
+        sorted_variables["immutable"].append(*args)
+    return sorted_variables
+    pass
+
+
+kor(lucky_number)
+kor(pi)
+kor(one_is_a_prime_number)
+kor(name)
+kor(my_favourite_films)
+kor(profile_info)
+kor(marks)
+kor(collection_of_coins)
