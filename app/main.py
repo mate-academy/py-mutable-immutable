288f3c1501c8774a5d67297f5a01cd2db07582e2
Iryna Ishchenko <irynaira@gmail.com>
2022-12-26 14:38:08 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,13 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+result = [lucky_number, pi, one_is_a_prime_number, name, my_favourite_films, collection_of_coins, profile_info, marks]
+cheng = []
+no_cheng = []
+for variable_name in result:
+    if isinstance(variable_name, (list, set, dict)):
+        cheng.append(variable_name)
+    elif isinstance(variable_name, (int, float, bool, str, tuple)):
+        no_cheng.append(variable_name)
+
+sorted_variables = {"mutable": cheng, "immutable": no_cheng}
