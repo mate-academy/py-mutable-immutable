71650774b163b07979a24815d89c7c66bf50f1dc
Daria Zayka <dariazayka@MacBook-Air-Daria.local>
2023-01-17 12:19:46 +0200

    Add solution for main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,12 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
-
-# write your code here
+def sorted_variables(*args) -> dict:
+    result = {}
+    mutable = []
+    immutable = []
+    for i in args:
+        if isinstance(i, (list, dict, set)):
+            mutable.append(i)
+        else:
+            immutable.append(i)
+    result["mutable"] = mutable
+    result["immutable"] = immutable
+    return result
