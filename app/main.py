ecfe845b90478525b641c261011b8732fd251647
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-01-23 14:38:22 +0200

    Update main.py
    The program sorts variables of different types. Program output: a dictionary with two keys: mutable and immutable, values are presented as lists.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,37 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
+#the dict_creation function creates a working dictionary from a list of variables of different types
+
+def dict_creation(**variable):
+    global dict_work
+    dict_work = dict(variable)
+    
+#I create an empty dictionary "sorted_variables" with the keys "mutable" and "immutable", the values 
+#of this dictionary will be in the form of lists
+
+sorted_variables = {"mutable": [], "immutable": []}
+
+dict_creation(lucky_number = 777,
+pi = 3.14,
+one_is_a_prime_number = False,
+name = "Richard",
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
+],
+profile_info = ("michel", "michel@gmail.com", "12345678"),
 marks = {
     "John": 4,
     "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+},
+collection_of_coins = {1, 2, 25})
 
-# write your code here
+#using the for loop, I sort through the dict_work dictionary and fill the sorted_variables dictionary
+
+for key,value in dict_work.items():
+    if isinstance (value, (list , set , dict)):
+        sorted_variables["mutable"].append(key)
+    else:
+        sorted_variables["immutable"].append(key)
+print(sorted_variables)
