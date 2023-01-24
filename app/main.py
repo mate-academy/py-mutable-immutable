7cc5e2e69a884a502287d060b33a16bb9ac1d36e
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-01-24 21:12:12 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,37 +1,60 @@
-#the dict_creation function creates a working dictionary from a list of variables of different types
-
-def dict_creation(**variable):
-    global dict_work
-    dict_work = dict(variable)
-    
-#I create an empty dictionary "sorted_variables" with the keys "mutable" and "immutable", the values 
-#of this dictionary will be in the form of lists
-
-sorted_variables = {"mutable": [], "immutable": []}
-
-dict_creation(lucky_number = 777,
-pi = 3.14,
-one_is_a_prime_number = False,
-name = "Richard",
+lucky_number = 777
+one_is_a_prime_number = False
+name = "Richard"
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
-],
-profile_info = ("michel", "michel@gmail.com", "12345678"),
+]
+profile_info = ("michel", "michel@gmail.com", "12345678")
 marks = {
     "John": 4,
     "Sergio": 3,
-},
-collection_of_coins = {1, 2, 25})
+}
+collection_of_coins = {1, 2, 25}
 
-#using the for loop, I sort through the dict_work dictionary and fill the sorted_variables dictionary
+sorted_variables = {"mutable": [], "immutable": []}
 
-for key,value in dict_work.items():
-    if isinstance (value, (list , set , dict)):
-        sorted_variables["mutable"].append(key)
-    else:
-        sorted_variables["immutable"].append(key)
+if isinstance (lucky_number, (list , set , dict)):
+    sorted_variables["mutable"].append("lucky_number")
+else:
+    sorted_variables["immutable"].append("lucky_number")
+    
+if isinstance (pi, (list , set , dict)):
+    sorted_variables["mutable"].append("pi")
+else:
+    sorted_variables["immutable"].append("pi")
+    
+if isinstance (one_is_a_prime_number, (list , set , dict)):
+    sorted_variables["mutable"].append("one_is_a_prime_number")
+else:
+    sorted_variables["immutable"].append("one_is_a_prime_number")
+    
+if isinstance (name, (list , set , dict)):
+    sorted_variables["mutable"].append("name")
+else:
+    sorted_variables["immutable"].append("name")
+    
+if isinstance (my_favourite_films, (list , set , dict)):
+    sorted_variables["mutable"].append("my_favourite_films")
+else:
+    sorted_variables["immutable"].append("my_favourite_films")
+    
+if isinstance (profile_info, (list , set , dict)):
+    sorted_variables["mutable"].append("profile_info")
+else:
+    sorted_variables["immutable"].append("profile_info")
+    
+if isinstance (marks, (list , set , dict)):
+    sorted_variables["mutable"].append("marks")
+else:
+    sorted_variables["immutable"].append("marks")
+    
+if isinstance (collection_of_coins, (list , set , dict)):
+    sorted_variables["mutable"].append("collection_of_coins")
+else:
+    sorted_variables["immutable"].append("collection_of_coins")
+    
 print(sorted_variables)
