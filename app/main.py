e7e20dbdd81170e361f3d7e94e934d710b5a036a
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-01-29 19:32:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,17 +15,14 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
 sorted_variables = {"mutable": [], "immutable": []}
-
 mutable = set, dict, list
 immutable = int, float, str, tuple, bool
-
-variables = [lucky_number, pi, one_is_a_prime_number, name, my_favourite_films, profile_info, marks, collection_of_coins]
-for var in variables:
-    if type(var) in mutable:
-        sorted_variables["mutable"].append(var)
+variables = [lucky_number, pi, one_is_a_prime_number, \
+name, my_favourite_films, profile_info, marks, collection_of_coins]
+for new_var in variables:
+    if type(new_var) in mutable:
+        sorted_variables["mutable"].append(new_var)
     else:
-        sorted_variables["immutable"].append(var)
-        
+        sorted_variables["immutable"].append(new_var)
 print(sorted_variables)
