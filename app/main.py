c6c043eb537a29c085a87527701231f999ad5646
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-02-01 18:08:21 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,35 +19,35 @@ sorted_variables = {"mutable": [], "immutable": []}
 mutable = set, dict, list
 immutable = int, float, str, tuple, bool
 if type(lucky_number) in mutable:
-    sorted_variables["mutable"].append("lucky_number")
+    sorted_variables["mutable"].append(lucky_number)
 else:
-    sorted_variables["immutable"].append("lucky_number")
+    sorted_variables["immutable"].append(lucky_number)
 if type(pi) in mutable:
-    sorted_variables["mutable"].append("pi")
+    sorted_variables["mutable"].append(pi)
 else:
-    sorted_variables["immutable"].append("pi")
+    sorted_variables["immutable"].append(pi)
 if type(one_is_a_prime_number) in mutable:
-    sorted_variables["mutable"].append("one_is_a_prime_number")
+    sorted_variables["mutable"].append(one_is_a_prime_number)
 else:
-    sorted_variables["immutable"].append("one_is_a_prime_number")
+    sorted_variables["immutable"].append(one_is_a_prime_number)
 if type(name) in mutable:
-    sorted_variables["mutable"].append("name")
+    sorted_variables["mutable"].append(name)
 else:
-    sorted_variables["immutable"].append("name")
+    sorted_variables["immutable"].append(name)
 if type(my_favourite_films) in mutable:
-    sorted_variables["mutable"].append("my_favourite_films")
+    sorted_variables["mutable"].append(my_favourite_films)
 else:
-    sorted_variables["immutable"].append("my_favourite_films")
+    sorted_variables["immutable"].append(my_favourite_films)
 if type(profile_info) in mutable:
-    sorted_variables["mutable"].append("profile_info")
+    sorted_variables["mutable"].append(profile_info)
 else:
-    sorted_variables["immutable"].append("profile_info")
+    sorted_variables["immutable"].append(profile_info)
 if type(marks) in mutable:
-    sorted_variables["mutable"].append("marks")
+    sorted_variables["mutable"].append(marks)
 else:
-    sorted_variables["immutable"].append("marks")
+    sorted_variables["immutable"].append(marks)
 if type(collection_of_coins) in mutable:
-    sorted_variables["mutable"].append("collection_of_coins")
+    sorted_variables["mutable"].append(collection_of_coins)
 else:
-    sorted_variables["immutable"].append("collection_of_coins")
+    sorted_variables["immutable"].append(collection_of_coins)
 print(sorted_variables)
