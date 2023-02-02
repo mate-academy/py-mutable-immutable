8867c737517148e89f639873390da5296235e6ad
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-02-02 21:27:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,5 +17,5 @@ marks = {
 collection_of_coins = {1, 2, 25}
 mutable = ["my_favourite_films, marks, collection_of_coins"]
 immutable = ["lucky_number, pi, one_is_a_prime_number, name, profile_info"]
-sorted_variables = {"mutable": mutable, "immutable" : immutable }
+sorted_variables = {"mutable": mutable, "immutable" : immutable}
 sorted_variables
