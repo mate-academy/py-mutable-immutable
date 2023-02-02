0f9f6c9b3ae96495888b0ab5ce3c91809a01df01
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-02-02 21:30:25 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-mutable = ["my_favourite_films, marks, collection_of_coins"]
-immutable = ["lucky_number, pi, one_is_a_prime_number, name, profile_info"]
+mutable = [my_favourite_films, marks, collection_of_coins]
+immutable = [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 sorted_variables = {"mutable": mutable, "immutable" : immutable}
 sorted_variables
