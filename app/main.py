5197555e33c0f42a958f2dee174691a056702943
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-05-22 15:03:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
-lucky_number = 777  # int
-pi = 3.14   # float
-one_is_a_prime_number = False   # boolean
-name = "Richard"    # string
-my_favourite_films = [  # list
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-profile_info = ("michel", "michel@gmail.com", "12345678")   # tuple
-marks = {   # dictionary
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
     "John": 4,
     "Sergio": 3,
 }
-collection_of_coins = {1, 2, 25}    # set
+collection_of_coins = {1, 2, 25}
 
 
 sorted_variables = {
