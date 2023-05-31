36a1a9d8de4fc521df30f543fa26910dd5571828
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-05-31 15:30:44 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -19,17 +19,22 @@ collection_of_coins = {1, 2, 25}
 sorted_variables = {"mutable": [], "immutable": []}
 
 
-def func(*args) -> dict:
+def obj_distributor(arg: object) -> dict:
 
     check_list = [list, dict, set]
 
-    for i in args:
-        if type(i) in check_list:
-            sorted_variables["mutable"].append(i)
-        else:
-            sorted_variables["immutable"].append(i)
+    if type(arg) in check_list:
+        sorted_variables["mutable"].append(arg)
+    else:
+        sorted_variables["immutable"].append(arg)
     return sorted_variables
 
 
-func(lucky_number, pi, one_is_a_prime_number, name, my_favourite_films,
-     profile_info, marks, collection_of_coins)
+obj_distributor(lucky_number)
+obj_distributor(pi)
+obj_distributor(one_is_a_prime_number)
+obj_distributor(name)
+obj_distributor(my_favourite_films)
+obj_distributor(profile_info)
+obj_distributor(marks)
+obj_distributor(collection_of_coins)
