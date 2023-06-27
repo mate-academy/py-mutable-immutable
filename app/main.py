fd153f8f75ff9f9ff14b517a272591b5c065e699
Yana Mushka <yanamushka@gmail.com>
2023-06-27 23:45:13 +0200

    не розумію, чому при тесті test_variables_should_be_added_to_sorted_variables[lucky_number] з sorted_vaiables витягується 777, якщо воно не додається туди в моєму коді

--- app/main.py
+++ app/main.py
@@ -16,4 +16,32 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+variables = [ ("lucky_number", 777),
+        ("pi", 3.14),
+        ("one_is_a_prime_number", False),
+        ("name", "Richard"),
+        (
+            "my_favourite_films",
+            [
+                "The Shawshank Redemption",
+                "The Lord of the Rings: The Return of the King",
+                "Pulp Fiction",
+                "The Good, the Bad and the Ugly",
+                "The Matrix",
+            ],
+        ),
+        ("profile_info", ("michel", "michel@gmail.com", "12345678")),
+        ("marks", {"John": 4, "Sergio": 3}),
+        ("collection_of_coins", {1, 2, 25}),
+    ]
+
+
+sorted_variables = {"mutable": [], "immutable": []}
+
+for i in variables:
+    if type(i[1]) in [int, float, str, bool, tuple]:
+        sorted_variables["immutable"].append(i[0])
+    else:
+        sorted_variables["mutable"].append(i[0])
+print(sorted_variables)
