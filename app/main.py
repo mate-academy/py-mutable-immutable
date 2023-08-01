115f0c58b46034a613a61bd51d456a0fc6a2b07f
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-08-01 16:15:48 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -18,14 +18,6 @@ collection_of_coins = {1, 2, 25}
 all_variables = [lucky_number, pi, one_is_a_prime_number, name,
                  my_favourite_films, profile_info, marks, collection_of_coins]
 sorted_variables = {
-    "mutable": [],
-    "immutable": []
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
-
-allowed_types = [int, float, str, bool, tuple]
-
-for var_type in all_variables:
-    if type(var_type) in allowed_types:
-        sorted_variables["immutable"].append(var_type)
-    else:
-        sorted_variables["mutable"].append(var_type)
