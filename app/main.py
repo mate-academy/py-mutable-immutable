66113ae27d26a79ec3f6f3df58fdff8b78bc4b38
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-01 14:47:01 +0300

    Sort variables

--- app/main.py
+++ app/main.py
@@ -16,4 +16,9 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+mutable_list = [my_favourite_films, marks, collection_of_coins]
+immutable_list = [lucky_number, pi, profile_info, one_is_a_prime_number, name]
+mutable_types = [list, set, dict]
+immutable_types = [int, float, str, bool, type(None), tuple]
+
+sorted_variables = {"mutable": mutable_list, "immutable": immutable_list}
