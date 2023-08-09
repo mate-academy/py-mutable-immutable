f0a5eded8cdfbd997fc137f2ab008e3e6d109d68
Denys Kibalenko <dennieking87@gmail.com>
2023-08-09 17:24:40 +0200

    Solution. Rewrite dict

--- app/main.py
+++ app/main.py
@@ -16,13 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-sorted_variables = {"immutable": [
-                    lucky_number,
-                    pi,
-                    one_is_a_prime_number,
-                    name, profile_info
-                    ],
-                    "mutable": [
-                    my_favourite_films,
-                    marks,
-                    collection_of_coins]}
+sorted_variables = {
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
+    "mutable": [my_favourite_films, marks, collection_of_coins]
+}
