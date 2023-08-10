bd81df610b5b1a71b20ae70847466366ed1a3adb
Taras_G <firsttaras@mail.com>
2023-08-10 17:43:42 +0300

    Names variables as items

--- app/main.py
+++ app/main.py
@@ -17,10 +17,6 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [["The Shawshank Redemption",
-                 "The Lord of the Rings: The Return of the King",
-                 "Pulp Fiction", "The Good, the Bad and the Ugly",
-                 "The Matrix"], {"John": 4, "Sergio": 3}, {1, 2, 25}],
-    "immutable": [777, 3.14, False, "Richard",
-                  ("michel", "michel@gmail.com", "12345678")]
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
