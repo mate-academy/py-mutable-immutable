91996170fc256bb3260bb3bf3376ffb8c4fbb4a2
Roman Filonenko <91filonenko@gmail.com>
2023-08-14 19:45:22 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,31 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-dictionary_variables = {
-    "mutable": [list, dict, set],
-    "immutable": [int, float, bool, str, tuple]
-}
-
 sorted_variables = {
-    "mutable": [],
-    "immutable": []
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
-
-for key, values in dictionary_variables.items():
-    for value in values:
-        if value == type(lucky_number):
-            sorted_variables[key].append(lucky_number)
-        if value == type(pi):
-            sorted_variables[key].append(pi)
-        if value == type(one_is_a_prime_number):
-            sorted_variables[key].append(one_is_a_prime_number)
-        if value == type(name):
-            sorted_variables[key].append(name)
-        if value == type(profile_info):
-            sorted_variables[key].append(profile_info)
-        if value == type(my_favourite_films):
-            sorted_variables[key].append(my_favourite_films)
-        if value == type(marks):
-            sorted_variables[key].append(marks)
-        if value == type(collection_of_coins):
-            sorted_variables[key].append(collection_of_coins)
