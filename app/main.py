461fc09755ddd5f3eafd50d7518914b1f4eafcd8
Oksana Kozlova <brunhildaaaaa@gmail.com>
2023-08-19 15:14:27 +0300

    'My_Solution'

--- app/main.py
+++ app/main.py
@@ -16,27 +16,23 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-sorted_variables = {
-    "mutable": [],
-    "immutable": []
-}
-
-
-def sort_variables(current_var: any) -> None:
-
-    if isinstance(current_var, (int, float, str, bool, tuple)):
-        sorted_variables["immutable"].append(current_var)
-    else:
-        sorted_variables["mutable"].append(current_var)
-
-
-sort_variables(lucky_number)
-sort_variables(pi)
-sort_variables(one_is_a_prime_number)
-sort_variables(name)
-sort_variables(my_favourite_films)
-sort_variables(profile_info)
-sort_variables(marks)
-sort_variables(collection_of_coins)
-
-print(sorted_variables)
+sorted_variables = dict(
+    mutable=[
+        [
+            "The Shawshank Redemption",
+            "The Lord of the Rings: The Return of the King",
+            "Pulp Fiction",
+            "The Good, the Bad and the Ugly",
+            "The Matrix",
+        ],
+        {"John": 4, "Sergio": 3},
+        {1, 2, 25}
+    ],
+    immutable=[
+        777,
+        3.14,
+        False,
+        "Richard",
+        ("michel", "michel@gmail.com", "12345678")
+    ]
+)
