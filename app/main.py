589fbdc3dc2cc99d579d1719926d577039338312
Alex <oleksa.shcherbakov@gmail.com>
2023-09-06 15:51:05 +0300

    solution_1.2

--- app/main.py
+++ app/main.py
@@ -21,20 +21,3 @@ sorted_variables = {
     "mutable" : [my_favourite_films, marks, collection_of_coins],
     "immutable" : [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
-
-#my_dict = {"lucky_number": lucky_number, "pi": pi, "one_is_a_prime_number": one_is_a_prime_number, "name": name, "my_favourite_films": my_favourite_films, "profile_info": profile_info, "marks": marks, "collection_of_coins": collection_of_coins }
-#sorted_variables = {}
-
-#for key, value in my_dict.items():
-#    if isinstance(value, (list, set, dict)):
-#        sorted_variables["mutable"] = []
-#    else:
-#        sorted_variables["immutable"] = []
-
-#for key, value in my_dict.items():
-#    if isinstance(value, (list, set, dict)):
-#        sorted_variables["mutable"] += [key]
-#    else:
-#        sorted_variables["immutable"] += [key]
-
-#print(sorted_variables)
