253963f2be9eaeebae409dd21e871b1c3396ac3d
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2023-09-22 18:32:19 +0300

    make my app simply

--- app/main.py
+++ app/main.py
@@ -16,15 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-all_data = [lucky_number, pi, one_is_a_prime_number, name, my_favourite_films, profile_info, marks, collection_of_coins]
-mut, immut = [], []
-
-for el in all_data:
-    if type(el) == int or type(el) == float or type(el) == str or type(el) == tuple or type(el) == bool:
-        immut.append(el)
-    else:
-        mut.append(el)
 sorted_variables = {
-    "mutable": mut,
-    "immutable": immut
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
