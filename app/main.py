50e4f130a5caeb1488f514692b3a9ab4bb971e39
naiv <nazarivankiv1@gmail.com>
2023-09-25 10:47:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,26 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Creating dictionary where results will be added
+sorted_variables = {
+    "mutable" : [],
+    "immutable": []}
+
+# Adding all variables to list to iterate through variables
+variables_list = [lucky_number,
+                  pi,
+                  one_is_a_prime_number,
+                  name,
+                  my_favourite_films,
+                  profile_info,
+                  marks,
+                  collection_of_coins]
+
+# Itterating thgough variables
+for item in variables_list:
+
+    # Checking if data type is mutable or immutable
+    if type(item) == list or type(item) == dict or type(item) == set:
+        sorted_variables["mutable"].append(item)
+    else:
+        sorted_variables["immutable"].append(item)
