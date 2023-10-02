48675f328dc715732f88462a797bbe49c4ce8125
Dmytro Semenets <dmitriymine@gmail.com>
2023-10-02 20:57:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -15,9 +15,10 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
+a = "collection_of_coins"
 mu = ["my_favourite_films", "profile_info", "marks"]
 
-im = ["lucky_number", "pi", "one_is_a_prime_number", "name", "collection_of_coins"]
+im = ["lucky_number", "pi", "one_is_a_prime_number", "name"]
+im.append(a)
 
 sorted_variables = {"mutable": mu, "immutable": im}
