4e74742ffe5e3b2c4b14f80b088451b6e484dbf2
Dmytro Semenets <dmitriymine@gmail.com>
2023-10-03 20:18:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,8 @@ collection_of_coins = {1, 2, 25}
 mu = [my_favourite_films, marks]
 
 im = [lucky_number, pi, one_is_a_prime_number, name]
-im.append(collection_of_coins)
 im.append(profile_info)
+im.append(collection_of_coins)
+
 
 sorted_variables = {"mutable": mu, "immutable": im}
