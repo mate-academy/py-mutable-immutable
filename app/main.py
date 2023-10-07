82d174807436ff897fa004caa2f73d8064295c8a
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-10-07 12:44:57 +0300

    flake8 fixes

--- app/main.py
+++ app/main.py
@@ -1,21 +1,9 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-
 def check_type(list_elem : list) -> dict:
     for_mutable = []
     for_immutable = []
     sorted_variables = {}
     for elem in list_elem:
-        if isinstance(elem,(int,str,tuple,float)):
+        if isinstance(elem, (int, str, tuple, float)):
             for_immutable.append(elem)
         else:
             for_mutable.append(elem)
