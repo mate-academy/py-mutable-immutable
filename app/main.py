35a1d7538927a14367ea0e589919906507729e14
Olga Dolnyk <dolnik2104@gmail.com>
2023-10-10 12:14:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -27,21 +27,3 @@ sorted_variables = {"mutable": [
                     one_is_a_prime_number,
                     name,
                     profile_info]}
-
-print({
-    "mutable": [
-        [
-            "The Shawshank Redemption",
-            "The Lord of the Rings: The Return of the King",
-            "Pulp Fiction",
-            "The Good, the Bad and the Ugly",
-            "The Matrix",
-        ],
-        {"John": 4, "Sergio": 3},
-        {1, 2, 25},
-    ],
-    "immutable": [777, 3.14, False, "Richard",
-                  ("michel",
-                   "michel@gmail.com",
-                   "12345678")],
-})
