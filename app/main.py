91d49d61f3d0edfcfbbe837479e96ef14d31277f
Liliya <liliyakhu@ukr.net>
2023-10-13 16:59:10 +0300

    Deleted extra lines

--- app/main.py
+++ app/main.py
@@ -16,20 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-list_of_variables = [lucky_number, pi, one_is_a_prime_number,
-                     name, my_favourite_films, profile_info,
-                     marks, collection_of_coins]
-list_of_mutable = []
-list_of_immutable = []
-sorted_variables = {"mutable": list_of_mutable,
-                    "immutable": list_of_immutable}
-for element in list_of_variables:
-    if isinstance(element, (list, set, dict)):
-        list_of_mutable.append(element)
-    else:
-        list_of_immutable.append(element)
-
-sorted_variables_manual = {
+sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
