0352bf829d4b359bbfbe8e97fcf8088e2af51ef6
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-10-28 11:56:30 +0300

    manually sorted the data types

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
-def check_type(list_elem : list) -> dict:
-    for_mutable = []
-    for_immutable = []
-    sorted_variables = {}
-    for elem in list_elem:
-        if isinstance(elem, (int, str, tuple, float)):
-            for_immutable.append(elem)
-        else:
-            for_mutable.append(elem)
-    sorted_variables["mutable"] = for_mutable
-    sorted_variables["immutable"] = for_immutable
-    return sorted_variables
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}
+collection_of_coins = {1, 2, 25}
+
+sorted_values = {'mutable' : [marks,collection_of_coins,my_favourite_films],
+                 'immutable' : [lucky_number,pi, name,
+                                one_is_a_prime_number,profile_info]}
+
+
+
