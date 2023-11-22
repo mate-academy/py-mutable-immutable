e843e3f1ef2679832e1bc4be64c888532afd0a73
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-11-22 20:56:38 +0100

    simplify the code.

--- app/main.py
+++ app/main.py
@@ -16,21 +16,9 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-variables = [lucky_number, pi, one_is_a_prime_number,
-             name, my_favourite_films, profile_info,
-             marks, collection_of_coins
-             ]
-immutable_var = (int, float, str, bool, tuple)
-immutable_list = []
-mutable_list = []
-for value in variables:
-    if type(value) in immutable_var:
-        immutable_list.append(value)
-    else:
-        mutable_list.append(value)
 sorted_variables = {
-    "mutable": mutable_list,
-    "immutable": immutable_list
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
 
 print(sorted_variables)
