d39c3a9bab1ba48120299f6299adfbd4817029b1
ruslan-sketch <118385742+ruslan-sketch@users.noreply.github.com>
2023-12-02 11:20:33 +0200

    flake8 fix

--- app/main.py
+++ app/main.py
@@ -19,5 +19,4 @@ collection_of_coins = {1, 2, 25}
 sorted_variables = {"mutable" : [marks, collection_of_coins,
                                  my_favourite_films],
                     "immutable" : [lucky_number, pi, name,
-                                  one_is_a_prime_number,
-                                  profile_info]}
+                                   one_is_a_prime_number, profile_info]}
