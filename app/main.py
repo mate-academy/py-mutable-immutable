2a76e41b46ac06ec94ef8594b9d3412dc4078f5c
M4Xpy <131768808+M4Xpy@users.noreply.github.com>
2023-12-12 21:18:06 +0200

    cancel changes in  main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,3 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # write your code here
-sorted_variables = {
-    "mutable": [my_favourite_films, marks, collection_of_coins],
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
-}
