2a280a5664648fa8ad4a0c8a533d44d26dd21435
Your_Name Your_Surname <your@email>
2023-12-15 23:41:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,9 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+mutable_value = [my_favourite_films, marks, collection_of_coins]
+immutable_value = [lucky_number, pi, name, one_is_a_prime_number, profile_info]
+sorted_variables = {
+    "mutable": mutable_value,
+    "immutable": immutable_value
+}
