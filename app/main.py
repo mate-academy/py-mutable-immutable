98c91f72d5edb9e3d1abdbdee6e83b09098d7e00
bogdankoziy <133056142+bogdankoziy@users.noreply.github.com>
2023-12-22 22:18:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,27 +16,6 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 sorted_variables = {
-    'mutable': [my_favourite_films, marks, collection_of_coins],
-    'immutable': [lucky_number, pi, one_is_a_prime_number, name, profile_info, ]
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info, ]
 }
-# def sort_args_type(**kwargs) -> dict[list]:
-#     # sorted_variables = {'mutable': [], 'immutable': []}
-#
-#     # Cycle for checking each arg type and putting to one of two lists
-#     for variable, value in kwargs:
-#         if (
-#                 str(type(value).__name__) == "int"
-#                 or str(type(value).__name__) == "float"
-#                 or str(type(value).__name__) == "tuple"
-#                 or str(type(value).__name__) == "bool"
-#                 or str(type(value).__name__) == "NoneType"
-#         ):
-#             sorted_variables['mutable'].append(value)
-#         elif (
-#                 str(type(value).__name__) == "list"
-#                 or str(type(value).__name__) == "dict"
-#                 or str(type(value).__name__) == "set"
-#         ):
-#             sorted_variables['immutable'].append(value)
-#
-#     return sorted_variables
