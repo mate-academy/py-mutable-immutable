c1c4f2573cd56424c21d51e7190963c6ddc4610f
bogdankoziy <133056142+bogdankoziy@users.noreply.github.com>
2023-12-22 12:41:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,5 +15,28 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
-# write your code here
+sorted_variables = {
+    'mutable': [my_favourite_films, marks, collection_of_coins],
+    'immutable': [lucky_number, pi, one_is_a_prime_number, name, profile_info, ]
+}
+# def sort_args_type(**kwargs) -> dict[list]:
+#     # sorted_variables = {'mutable': [], 'immutable': []}
+#
+#     # Cycle for checking each arg type and putting to one of two lists
+#     for variable, value in kwargs:
+#         if (
+#                 str(type(value).__name__) == "int"
+#                 or str(type(value).__name__) == "float"
+#                 or str(type(value).__name__) == "tuple"
+#                 or str(type(value).__name__) == "bool"
+#                 or str(type(value).__name__) == "NoneType"
+#         ):
+#             sorted_variables['mutable'].append(value)
+#         elif (
+#                 str(type(value).__name__) == "list"
+#                 or str(type(value).__name__) == "dict"
+#                 or str(type(value).__name__) == "set"
+#         ):
+#             sorted_variables['immutable'].append(value)
+#
+#     return sorted_variables
