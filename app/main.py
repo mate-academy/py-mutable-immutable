1980eb49b3581806300fdd7e918853fc41d9d608
Maksym Hekalo <maximgekalo@gmail.com>
2024-01-08 22:26:22 +0200

    fix implementation, without loop

--- app/main.py
+++ app/main.py
@@ -16,22 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-list_of_different_type_variable = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    my_favourite_films,
-    profile_info,
-    marks,
-    collection_of_coins
-]
-
-sorted_variables = {"mutable": [], "immutable": []}
-mutable_data = [dict, list, set]
-for data in list_of_different_type_variable:
-    type_of_variable = type(data)
-    if type_of_variable in mutable_data:
-        sorted_variables["mutable"].append(data)
-        continue
-    sorted_variables["immutable"].append(data)
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
