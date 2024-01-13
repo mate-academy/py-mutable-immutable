49757017e900ed587e1ff9bdff519a3c8cc39272
Andriy Shchur <scandriy@gmail.com>
2024-01-13 13:03:49 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ collection_of_coins = {1, 2, 25}
 sorted_variables = {
     "mutable": [
         my_favourite_films,
-        profile_info,
         marks,
         collection_of_coins
     ],
@@ -27,6 +26,7 @@ sorted_variables = {
         lucky_number,
         pi,
         one_is_a_prime_number,
-        name
+        name,
+        profile_info
     ]
 }
