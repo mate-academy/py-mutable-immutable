90022f1e620c3d261905928cb59ee5297f2575da
Max Yevtushyk <yevtushyk.architect@gmail.com>
2024-02-05 23:01:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -16,24 +16,8 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-variable_name = {"lucky_number": 777,
-                 "pi": 3.14,
-                 "one_is_a_prime_number": False,
-                 "name": "Richard",
-                 "my_favourite_films": [
-                     "The Shawshank Redemption",
-                     "The Lord of the Rings: The Return of the King",
-                     "Pulp Fiction",
-                     "The Good, the Bad and the Ugly",
-                     "The Matrix",
-                 ],
-                 "profile_info": ("michel", "michel@gmail.com", "12345678"),
-                 "marks": {
-                     "John": 4,
-                     "Sergio": 3,
-                 },
-                 "collection_of_coins": {1, 2, 25}}
 
+local_variables = locals().copy()
 mutable_list = []
 immutable_list = []
 
@@ -42,8 +26,12 @@ sorted_variables = {
     "immutable": immutable_list
 }
 
-for i in variable_name.values():
-    if type(i) is dict or type(i) is set or type(i) is list:
-        mutable_list.append(i)
+for key, value in local_variables.items():
+    if key.startswith("__") and key.endswith("__"):
+        continue
+    if isinstance(value, list) or isinstance(value, dict) or isinstance(value, set):
+        mutable_list.append(value)
     else:
-        immutable_list.append(i)
+        immutable_list.append(value)
+
+print(sorted_variables)
