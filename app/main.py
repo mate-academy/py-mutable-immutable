ac837494c5323c8d32be6fd3d170ef3902cddc94
Klishch Nazar <klishch65@gmail.com>
2024-02-21 20:22:24 +0200

    Delete comments

--- app/main.py
+++ app/main.py
@@ -16,20 +16,6 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# def create_dictionary(*args) -> dict:
-#     sorted_variables = {
-#         "mutable": [],
-#         "immutable": []
-#     }
-#
-#     for arg in args:
-#         if isinstance(arg, (list, dict, set)):
-#             sorted_variables["mutable"].append(arg)
-#         else:
-#             sorted_variables["immutable"].append(arg)
-#
-#     return sorted_variables
-
 sorted_variables = {
     "mutable": [collection_of_coins, my_favourite_films, marks],
     "immutable": [profile_info, name, one_is_a_prime_number, pi, lucky_number],
