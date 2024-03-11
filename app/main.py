fb30e91ce9c17f7fa2dd93d4905ae32513ae3cc3
Andrii Chubenko <stalkerock@gmail.com>
2024-03-11 12:02:00 +0200

    Better formating

--- app/main.py
+++ app/main.py
@@ -20,11 +20,13 @@ sorted_variables = {
     "mutable": [
         my_favourite_films,
         marks,
-        collection_of_coins],
+        collection_of_coins
+    ],
     "immutable": [
         lucky_number,
         pi,
         one_is_a_prime_number,
         name,
-        profile_info]
+        profile_info
+    ]
 }
