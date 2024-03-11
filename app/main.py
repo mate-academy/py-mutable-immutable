8b1984cde1b67fb9aef2dc1bfafc2a79bc0831bc
bujhm <Bujhm99@gmail.com>
2024-03-11 17:47:20 +0200

    dictionary not list,  variable not var,

--- app/main.py
+++ app/main.py
@@ -17,16 +17,16 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 
-def sort_var(*args) -> dict:
-    return_list = {"mutable": [], "immutable": []}
+def sort_variable(*args) -> dict:
+    return_dict = {"mutable": [], "immutable": []}
     for obj in args:
         if isinstance(obj, (list, dict, set)):
-            return_list["mutable"].append(obj)
+            return_dict["mutable"].append(obj)
         else:
-            return_list["immutable"].append(obj)
-    return return_list
+            return_dict["immutable"].append(obj)
+    return return_dict
 
 
-sorted_variables = sort_var(lucky_number, pi, one_is_a_prime_number,
-                            name, my_favourite_films, profile_info,
-                            marks, collection_of_coins)
+sorted_variables = sort_variable(lucky_number, pi, one_is_a_prime_number,
+                                 name, my_favourite_films, profile_info,
+                                 marks, collection_of_coins)
