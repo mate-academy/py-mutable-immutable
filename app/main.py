f5d3c8197ddabe6e76c86a0dc2322b9dc4fd876d
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-03-29 19:40:21 +0200

    more simple code according to the Vasyl Smutok comment

--- app/main.py
+++ app/main.py
@@ -21,14 +21,17 @@ def _variable_sorter(*args) -> dict:
 
     result = {"mutable": [], "immutable": [], }
 
-    for var_name in [_ for _ in args[0] if not _.startswith("_")]:
-        if (type(eval(var_name)) is dict or type(eval(var_name)) is list
-                or type(eval(var_name)) is set):
-            result["mutable"].append(eval(var_name))
+    for var_name in args:
+        if (isinstance(var_name, set) or isinstance(var_name, dict)
+                or isinstance(var_name, list)):
+            result["mutable"].append(var_name)
             continue
-        result["immutable"].append(eval(var_name))
+        result["immutable"].append(var_name)
 
     return result
 
 
-sorted_variables = _variable_sorter(dir())
+sorted_variables = _variable_sorter(
+    lucky_number, pi, one_is_a_prime_number, marks,
+    name, my_favourite_films, profile_info, collection_of_coins
+)
