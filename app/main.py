337b244badb63b0f577b6237ec7c8b9c6f1d5aff
AnnaAAA3432 <144123194+AnnaAAA3432@users.noreply.github.com>
2024-04-08 12:31:02 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -29,8 +29,8 @@ for var_name, var_value in global_vars_snapshot.items():
             immutable_variables.append(var_value)
 
 sorted_variables = {
-    "mutable": mutable_variables,
-    "immutable": immutable_variables
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
 
 print(sorted_variables)
