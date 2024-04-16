9c1eb1b65939a64caf1634d5c5a9fa1cafc1fd3e
Yakov K <hyakunosuke7@gmail.com>
2024-04-16 19:30:25 +0300

    MuIm Solution UPD

--- app/main.py
+++ app/main.py
@@ -20,12 +20,11 @@ sorted_variables = {"mutable": [], "immutable": []}
 
 for var_name in dir():
     if not var_name.startswith("__"):
-        var = globals()[var_name]
-        if isinstance(var, (list, dict, set)):
-            sorted_variables["mutable"].append(var)
-        elif isinstance(var, (int, float, bool, str, tuple)):
-            sorted_variables["immutable"].append(var)
+        current_var = globals()[var_name]
+        if isinstance(current_var, (list, dict, set)):
+            sorted_variables["mutable"].append(current_var)
+        elif isinstance(current_var, (int, float, bool, str, tuple)):
+            sorted_variables["immutable"].append(current_var)
 
 print(sorted_variables)
-
 #  Якщо випадково неправильно зрозумiв завдання, будь ласка вiдпишiть в git
