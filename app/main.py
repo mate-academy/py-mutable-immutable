2dfddb776ad5601d7c9020d9bf050f08422b9f45
Alex Troianovskiy <168637534+Alex93Troy@users.noreply.github.com>
2024-05-04 15:02:37 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-"""creat func sorted_variables"""
-
-lucky_number, pi, one_is_a_prime_number, name = 777, 3.14, False, "Richard"
-
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
@@ -14,46 +14,8 @@ marks = {
     "John": 4,
     "Sergio": 3,
 }
-collection_of_coins = {1, 2, 25}
 
-
-def sorted_variables(*args, **kwargs) -> dict:
-    """
-    create dictioanry with with mutable and immutable value
-    """
-    result_dict = {"mutable": [], "immutable": []}
-    lst_args = [args] + [kwargs]
-    for el in lst_args:
-        if isinstance(el, (int, float, str, tuple, bool)):
-            if isinstance(el, int):
-                result_dict.setdefault("immutable", []).append("lucky_number")
-            elif isinstance(el, float):
-                result_dict.setdefault("immutable", []).append("pi")
-            elif isinstance(el, str):
-                result_dict.setdefault("immutable", []).append("name")
-            elif isinstance(el, bool):
-                (result_dict.setdefault("immutable", []).append
-                 ("one_is_a_prime_number"))
-            if isinstance(el, tuple):
-                result_dict.setdefault("immutable", []).append("profile_info")
-
-        else:
-            if isinstance(el, list):
-                (result_dict.setdefault("mutable", []).append
-                 ("my_favourite_films"))
-            elif isinstance(el, dict):
-                result_dict.setdefault("mutable", []).append("marks")
-            elif isinstance(el, set):
-                (result_dict.setdefault("mutable", []).
-                 append("collection_of_coins"))
-    return sorted(result_dict)
-
-
-sorted_variables(lucky_number,
-                 pi,
-                 one_is_a_prime_number,
-                 name,
-                 my_favourite_films,
-                 profile_info,
-                 marks,
-                 collection_of_coins)
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
