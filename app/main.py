00ab749ee6dfb57b1b9ff1b4b7be0ee14d0bf128
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-05-08 10:21:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,25 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+def sorting_variables(*args) -> None:
+
+    mutable_list = []
+    immutable_list = []
+
+    for argument in args:
+
+        if isinstance(argument, (list, set, dict)):
+            mutable_list.append(argument)
+        else:
+            immutable_list.append(argument)
+
+    sorted_variables["immutable"] = immutable_list
+    sorted_variables["mutable"] = mutable_list
+
+
+sorted_variables = {}
+sorting_variables(lucky_number, pi, one_is_a_prime_number, name,
+                  my_favourite_films, profile_info, marks, collection_of_coins)
+
+print(sorted_variables)
