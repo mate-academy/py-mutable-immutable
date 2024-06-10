40ffd8720b59c8b11c934d90ab74b160fc768912
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-10 15:03:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# Дані змінні
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
@@ -16,4 +17,24 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+def classify_variables(*args) -> dict:
+    mutable_types = (list, set, dict)
+    immutable_types = (int, float, str, bool, type(None), tuple)
+
+    result = {
+        "mutable": [],
+        "immutable": []
+    }
+
+    for arg in args:
+        if isinstance(arg, mutable_types):
+            result["mutable"].append(arg)
+        elif isinstance(arg, immutable_types):
+            result["immutable"].append(arg)
+
+    return result
+
+sorted_variables = classify_variables(
+    lucky_number, pi, one_is_a_prime_number, name, my_favourite_films,
+    profile_info, marks, collection_of_coins
+)
