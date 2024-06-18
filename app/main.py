f10e40fadf3a80d00160dee23ced94627d0db996
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-06-18 11:46:50 +0300

    add new name for all variables and create a type check for mutable and immutable variables

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
+var1 = lucky_number = 777
+var2 = pi = 3.14
+var3 = one_is_a_prime_number = False
+var4 = name = "Richard"
+var5 = my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
+var6 = profile_info = ("michel", "michel@gmail.com", "12345678")
+var7 = marks = {
     "John": 4,
     "Sergio": 3,
 }
-collection_of_coins = {1, 2, 25}
+var8 = collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+
+
+def is_mutable(obj):
+    mutable_types = (list, dict, set)
+    return isinstance(obj, mutable_types)
+
+
+for var in (var1, var2, var3, var4, var5, var6, var7, var8):
+    if is_mutable(var):
+        sorted_variables["mutable"].append(var)
+    else:
+        sorted_variables["immutable"].append(var)
+
+print(sorted_variables)
