2ad5f780b0c63cfcafb356405c2cb030554e42da
Oleksandra <korzhaniuko@gmail.com>
2024-06-27 10:18:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -20,4 +20,4 @@ sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, profile_info, name],
 }
-print(sorted_variables)
+
