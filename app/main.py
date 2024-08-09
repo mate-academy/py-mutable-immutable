a6e1e314ed08a1eff492510316093348f869a6a7
Mike Shmygel <m.shmygel@gmail.com>
2024-08-10 02:26:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
 name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
+my_favourite_films = \
+    [
+        "The Shawshank Redemption",
+        "The Lord of the Rings: The Return of the King",
+        "Pulp Fiction",
+        "The Good, the Bad and the Ugly",
+        "The Matrix",
+    ]
 profile_info = ("michel", "michel@gmail.com", "12345678")
 marks = {
     "John": 4,
@@ -16,4 +17,20 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {"mutable": [], "immutable": []}
+variables = [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+]
+
+for variance in variables:
+    if type(variance) in [list, dict, set]:
+        sorted_variables["mutable"].append(variance)
+    else:
+        sorted_variables["immutable"].append(variance)
