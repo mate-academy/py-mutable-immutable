e699369b73bf4be6c49e41442a841f18d94cb745
Mike Shmygel <m.shmygel@gmail.com>
2024-08-10 16:35:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -17,20 +17,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-sorted_variables = {"mutable": [], "immutable": []}
-variables = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    my_favourite_films,
-    profile_info,
-    marks,
-    collection_of_coins
-]
-
-for variance in variables:
-    if type(variance) in [list, dict, set]:
-        sorted_variables["mutable"].append(variance)
-    else:
-        sorted_variables["immutable"].append(variance)
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
