b5f30240924c2590d175a90fb45f215500e12d98
Yurii <yurazosia@gmail.com>
2024-08-22 09:04:02 +0300

    Update app/main.py
    Co-authored-by: Vasyl Smutok <vasyl.smutok@gmail.com>

--- app/main.py
+++ app/main.py
@@ -16,31 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-variable_names = [
-    "lucky_number",
-    "pi",
-    "one_is_a_prime_number",
-    "name",
-    "my_favourite_films",
-    "profile_info",
-    "marks",
-    "collection_of_coins",
-]
-
-# --------------- Варіант просто з назвами перемінних ---------------------- #
-# sorted_variables = {
-#     "mutable": ["my_favourite_films", "marks", "collection_of_coins"],
-#     "immutable": ["lucky_number", "pi", "one_is_a_prime_number", "name",
-#     "profile_info"]
-# }
-
-# -------------------- Варіант зі значеннями перемінних  ------------------- #
-# ------------------- з обмеженням Flake8 у 79 символів на строку -----------#
 sorted_variables = {
-    "mutable": [["The Shawshank Redemption",
-                 "The Lord of the Rings: The Return of the King",
-                 "Pulp Fiction", "The Good, the Bad and the Ugly",
-                 "The Matrix"], {"John": 4, "Sergio": 3}, {1, 2, 25}],
-    "immutable": [777, 3.14, False, "Richard",
-                  ("michel", "michel@gmail.com", "12345678")]
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
 }
