635d59888231ce20f6d223b8d1cf3621e4239930
Misha Kiblyk <mishka3401@gmail.com>
2024-08-24 01:39:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -31,27 +31,3 @@ sorted_variables = {
         profile_info,  # tuple
     ]
 }
-
-
-
-# Перевірка, чи 1 є простим числом
-is_prime = lambda n: n > 1 and all(n % i != 0 for i in range(2, int(n ** 0.5) + 1))
-
-# Виконання перевірок і друк значень
-if is_prime(1):
-    print("1 is not a prime number (This is correct).")
-else:
-    print("1 is a prime number (This is incorrect).")
-
-print(f"Richard's first favorite film: {my_favourite_films[0]}")
-print(f"Number of films in the list: {len(my_favourite_films)}")
-
-print(f"Michel's username: {profile_info[0]}")
-print(f"Michel's email: {profile_info[1]}")
-
-print(f"John's marks: {marks['John']}")
-print(f"Average marks (assuming numerical values): {sum(marks.values()) / len(marks)}")
-
-print(f"Unique coin values: {collection_of_coins}")
-total_coin_value = sum(collection_of_coins)
-print(f"Total value of coins: {total_coin_value}")
