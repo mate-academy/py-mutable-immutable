c8450b6ee9644f5eeb264a3a585a177055741cb4
MTCH <dimamitich2@gmail.com>
2024-08-27 16:58:54 +0300

    New solution without functions

--- app/main.py
+++ app/main.py
@@ -17,25 +17,18 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [],          # list, dict, set
-    "immutable": []         # int, float, str, tuple, bool
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins
+    ],
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info
+    ]
 }
-
-all_variables = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    my_favourite_films,
-    profile_info,
-    marks,
-    collection_of_coins
-]
-
-for check_variable in all_variables:
-    if isinstance(check_variable, (list, dict, set)):
-        sorted_variables["mutable"].append(check_variable)
-    else:
-        sorted_variables["immutable"].append(check_variable)
 
 print(sorted_variables)
