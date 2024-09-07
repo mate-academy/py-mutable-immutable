072b039d55f60e6d2cc833d71a61fd40a7881447
Radeon <egorka22e40x@gmail.com>
2024-09-07 21:32:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,14 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+variables = [
+    lucky_number, pi, one_is_a_prime_number, name, my_favourite_films,
+    profile_info, marks, collection_of_coins]
+mutable_obj = ("list", "dict", "set")
+sorted_variables = {"mutable": [], "immutable": []}
+
+for var_def in variables:
+    if type(var_def).__name__ in mutable_obj:
+        sorted_variables["mutable"].append(var_def)
+    else:
+        sorted_variables["immutable"].append(var_def)
