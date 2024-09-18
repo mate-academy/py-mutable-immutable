f4e5e9cb9ab272211fbd286ba49b4640b64b49d7
Anton <1r0nt0ny1970@gmail.com>
2024-09-18 21:52:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+a: int = 123
+b: list = []
+c: str = "Hi!"
+d: list = [1, 2]
 
-# write your code here
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+
+variables = [a, b, c, d]
+
+for var in variables:
+    if isinstance(var, (list, dict, set)):
+        sorted_variables["mutable"].append(var)
+    else:
+        sorted_variables["immutable"].append(var)
+
+print(sorted_variables)
