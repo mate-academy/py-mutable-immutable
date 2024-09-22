f1f729004ee1e55f0afcb71eed0b45fae7498e4c
Kateryna Tsygankova <kateryna.tsygankova@gmail.com>
2024-09-22 10:10:06 +0300

    Corrected solution acc. to comments

--- app/main.py
+++ app/main.py
@@ -16,17 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-variables = {
-    "lucky_number": lucky_number,
-    "pi": pi,
-    "one_is_a_prime_number": one_is_a_prime_number,
-    "name": name,
-    "my_favourite_films": my_favourite_films,
-    "profile_info": profile_info,
-    "marks": marks,
-    "collection_of_coins": collection_of_coins,
-}
-
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]}
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
