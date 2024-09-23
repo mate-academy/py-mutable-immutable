1b9f4df01d295e5f6ec71a7d703ee668a4d192aa
Matvii Safonov <matveysafonov2@gmail.com>
2024-09-24 00:31:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,36 +1,18 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+a = 123
+b = []
+c = "Hi!"
+d = [1, 2]
 
+variables = [
+    a,
+    b,
+    c,
+    d
+]
 sorted_variables = {
     "mutable": [],
     "immutable": [],
 }
-
-variables = [
-    lucky_number,
-    pi,
-    one_is_a_prime_number,
-    name,
-    my_favourite_films,
-    profile_info,
-    marks,
-    collection_of_coins
-]
 
 for current_variable in variables:
     if isinstance(current_variable, (list, dict, set)):
