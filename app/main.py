07ee0e99ed7a93581dbdb017f75ca199af357af5
Matvii Safonov <matveysafonov2@gmail.com>
2024-09-24 12:19:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,29 @@
-a = 123
-b = []
-c = "Hi!"
-d = [1, 2]
-
-variables = [
-    a,
-    b,
-    c,
-    d
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
 ]
-sorted_variables = {
-    "mutable": [],
-    "immutable": [],
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
 }
+collection_of_coins = {1, 2, 25}
 
-for current_variable in variables:
-    if isinstance(current_variable, (list, dict, set)):
-        sorted_variables["mutable"].append(current_variable)
-    else:
-        sorted_variables["immutable"].append(current_variable)
-
-print(sorted_variables)
+sorted_variables = {
+    "mutable": [my_favourite_films,
+                marks,
+                collection_of_coins],
+    "immutable": [pi,
+                  lucky_number,
+                  one_is_a_prime_number,
+                  name,
+                  profile_info,
+                  ],
+}
