d6f556fdba6c6d88e7275dd31988732db499e860
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2024-10-02 13:04:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,15 +16,30 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
-sorted_variables = {
-    "mutable": [],
-    "immutable": []
-}
+
+def categorize_variables() -> dict[str, list]:
+    sorted_variables = {
+        "mutable": [],
+        "immutable": []
+    }
+
+    def add_var_to_category(variable_data: any) -> None:
+        if isinstance(variable_data, (list, dict, set)):
+            sorted_variables["mutable"].append(variable_data)
+        else:
+            sorted_variables["immutable"].append(variable_data)
+
+    add_var_to_category(lucky_number)
+    add_var_to_category(pi)
+    add_var_to_category(one_is_a_prime_number)
+    add_var_to_category(name)
+    add_var_to_category(my_favourite_films)
+    add_var_to_category(profile_info)
+    add_var_to_category(marks)
+    add_var_to_category(collection_of_coins)
+
+    return sorted_variables
 
 
-def add_variable_to_category(variable):
-    if isinstance(variable, (list, dict, set)):
-        sorted_variables["mutable"].append(variable)
-    else:
-        sorted_variables["immutable"].append(variable)
+sorted_variables = categorize_variables()
+print(sorted_variables)
