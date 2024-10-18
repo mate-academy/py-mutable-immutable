64de64c01752d627fdf7e430ea45897219470f3a
4ikkamony <compbanderloga@gmail.com>
2024-10-18 19:03:52 +0300

    Soulution, but the comments are removed

--- app/main.py
+++ app/main.py
@@ -16,32 +16,6 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-"""
-# parse the names of first n variables in a file
-def get_n_variable_names(file, n):
-    try:
-        with open(file, "r") as f:
-            variables = []
-            while len(variables) != n:
-                line = f.readline().split(" = ")[0]
-                if line[0].isalpha() and "import" not in line:
-                    variables.append(line)
-            return variables
-    except FileNotFoundError:
-        print(f"{file} not found")
-        return None
-
-variable_names = get_n_variable_names("main.py", 8)
-"""
-
-"""
-In order for the above approach to pass the tests,
-we really shouldn't read the file while tests also try to do that
-
-So the solution is to either move the code to other file
-Or run it without tests, get the variable names
-And set them explicitly.
-"""
 variable_names = [
     "lucky_number",
     "pi",
