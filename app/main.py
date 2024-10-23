d37e8d613389e54e40d4e0d6cf6d338fadb4ed3e
Vitaliy Kozovyi <kozovyivitaliy@gmail.com>
2024-10-23 21:43:54 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,22 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Initialize dictionary to store mutable and immutable variables
+sorted_variables = {"mutable": [],
+                    "immutable": []}
+
+
+def sort_variables(*args) -> dict:
+    # Define mutable types
+    mutable_types = (list, dict, set)
+    # Sort variables as mutable or immutable
+    for element in args:
+        if isinstance(element, mutable_types):
+            sorted_variables["mutable"].append(element)
+        else:
+            sorted_variables["immutable"].append(element)
+    return sorted_variables
+
+
+sort_variables(lucky_number, pi, one_is_a_prime_number, name,
+               my_favourite_films, profile_info, marks, collection_of_coins)
