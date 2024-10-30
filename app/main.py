27067d41c2275b2d47d9ee056d5c8101dcde1454
Fallen Hero <failent.hero@gmail.com>
2024-10-30 12:16:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,11 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+variables_name_ls = [lucky_number, pi, one_is_a_prime_number, name, my_favourite_films, profile_info, marks, collection_of_coins]
+sorted_variables = {"mutable": [], "immutable": []}
+for var_name in variables_name_ls:
+    if isinstance(var_name, (list, dict, set)):
+        sorted_variables["mutable"].append(var_name)
+    else:
+        sorted_variables["immutable"].append(var_name)
+print(sorted_variables)
