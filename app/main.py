5e87986996e5c3cd7f7efdda236dbfb9e6ea5375
Dmytro Bulavka <dgbulavka@gmail.com>
2024-11-12 17:55:47 +0200

    fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,29 +1,37 @@
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
-name = "Richard"
+name = 'Richard'
 my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
+    'The Shawshank Redemption',
+    'The Lord of the Rings: The Return of the King',
+    'Pulp Fiction',
+    'The Good, the Bad and the Ugly',
+    'The Matrix',
 ]
-profile_info = ("michel", "michel@gmail.com", "12345678")
+profile_info = ('michel', 'michel@gmail.com', '12345678')
 marks = {
-    "John": 4,
-    "Sergio": 3,
+    'John': 4,
+    'Sergio': 3,
 }
 collection_of_coins = {1, 2, 25}
 
-sorted_variables = {"mutable": [], "immutable": []}
+sorted_variables = {'mutable': [], 'immutable': []}
 
-for variables in [lucky_number, pi, one_is_a_prime_number, name, my_favourite_films,
-            profile_info, marks, collection_of_coins]:
+for variables in [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+]:
 
     if isinstance(variables, (list, set, dict)):
-        sorted_variables["mutable"].append(variables)
+        sorted_variables['mutable'].append(variables)
     else:
-        sorted_variables["immutable"].append(variables)
+        sorted_variables['immutable'].append(variables)
 
 print(sorted_variables)
