2f1078589ec5f75fb270ad966a8f12e16bd7d634
Koziakov Hlib <hlibs.work@gmail.com>
2024-11-19 13:29:15 +0200

    Complete

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
-sorted_variables = {"mutable": [my_favourite_films, marks, collection_of_coins, profile_info],
-                    "immutable": [lucky_number, pi, one_is_a_prime_number, name]}
+mutable = [my_favourite_films, marks, collection_of_coins, profile_info]
+immutable = [lucky_number, pi, one_is_a_prime_number, name]
+sorted_variables = {"mutable": mutable,
+                    "immutable": immutable}
