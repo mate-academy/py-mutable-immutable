f799fc022fcac46c6525ca2ad3113330e4201c11
Yurii Lesyk <lesik.yurko@gmail.com>
2024-11-19 16:22:08 +0200

    Add solution for mutable-immutable task

---
+++ main.py
@@ -0,0 +1,41 @@
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}
+collection_of_coins = {1, 2, 25}
+
+# write your code here
+variables = [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins,
+]
+
+# Визначення типів
+mutable_types = (list, dict, set)
+immutable_types = (int, float, bool, str, tuple)
+
+# Автоматичне сортування змінних
+sorted_variables = {
+    "mutable": [var for var in variables if isinstance(var, mutable_types)],
+    "immutable": [var for var in variables if isinstance(var, immutable_types)],
+}
+
+print(sorted_variables)
