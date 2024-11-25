0d5941a31805c8901f4f77372987af12b9f71eef
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-11-25 14:24:52 +0200

    create mutable and immutable dictionary

--- app/main.py
+++ app/main.py
@@ -16,4 +16,25 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {
+    "mutable": [],
+    "immutable": [],
+}
+
+# Перевірка кожної змінної та її типу
+variables = {
+    "lucky_number": lucky_number,
+    "pi": pi,
+    "one_is_a_prime_number": one_is_a_prime_number,
+    "name": name,
+    "my_favourite_films": my_favourite_films,
+    "profile_info": profile_info,
+    "marks": marks,
+    "collection_of_coins": collection_of_coins,
+}
+
+for var_name, var_value in variables.items():
+    if isinstance(var_value, (int, float, bool, str, tuple)):
+        sorted_variables["immutable"].append(var_value)
+    elif isinstance(var_value, (list, dict, set)):
+        sorted_variables["mutable"].append(var_value)
