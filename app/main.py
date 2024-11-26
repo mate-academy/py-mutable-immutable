7d962bc237d59b6c44c709424058ab65b40e28b7
Lisa <zhyganiuk.elizaveta@gmail.com>
2024-11-26 14:28:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,19 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+def types_variables(variables):
+    sorted_variables = {
+        "mutable": [],
+        "immutable": []
+    }
+
+    for variable in variables:
+        if isinstance(variable, tuple):
+            if all(not isinstance(el, list) for el in variable):
+                sorted_variables["immutable"].append(variable)
+            else:
+                sorted_variables["mutable"].append(variable)
+        elif isinstance(variable, list):
+            sorted_variables["mutable"].append(variable)
+
+    return sorted_variables
