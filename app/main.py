09dc880d216048bc3ede664cf9fe5c47d11e7c9d
navnishan <navnishann@gmail.com>
2024-12-24 19:47:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,13 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+def sort_variables_by_mutability():
+    mutable_variables = [my_favourite_films, marks, collection_of_coins]  # Lists, dictionaries, and sets are mutable
+    immutable_variables = [lucky_number, pi, one_is_a_prime_number, name,
+                           profile_info]  # Integers, floats, booleans, strings, and tuples are immutable
+
+    return {"mutable": mutable_variables, "immutable": immutable_variables}
+
+
+sorted_variables = sort_variables_by_mutability()
+print(sorted_variables)
