3737d8e6aecadc7ccb600ce1e7cb29506913d357
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-01-06 18:47:44 +0200

    Created dict sorted_variables

--- app/main.py
+++ app/main.py
@@ -16,4 +16,8 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, name, one_is_a_prime_number, profile_info]
+}
