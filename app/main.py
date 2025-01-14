2cdec3f4611ea0372467809d915a84f99df8b08e
rediklayer <www.reddy12@gmail.com>
2025-01-14 21:32:54 +0300

    task solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,23 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+for variable in [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+]:
+    if type(variable) in [list, dict, set]:
+        sorted_variables["mutable"].append(variable)
+    else:
+        sorted_variables["immutable"].append(variable)
+
+print(sorted_variables)
