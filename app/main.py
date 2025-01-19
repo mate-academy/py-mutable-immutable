196421e9b870ced9a1a05705b77de65c0a021f8d
Vlad Bukin <vbukinnn2009@gmail.com>
2025-01-19 11:27:24 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from flake8.processor import mutate_string
+mutable_types = (list,dict,set)
+immutable_types = (int,str,tuple,float,bool)
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
@@ -15,5 +18,14 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
-# write your code here
+sorted_variables = {
+    "mutable": [var for var in [
+        lucky_number, pi, one_is_a_prime_number, name,
+        my_favourite_films, profile_info, marks, collection_of_coins
+    ] if isinstance(var, mutable_types)],
+    "immutable": [var for var in [
+        lucky_number, pi, one_is_a_prime_number, name,
+        my_favourite_films, profile_info, marks, collection_of_coins
+    ] if isinstance(var, immutable_types)],
+}
+print(sorted_variables)
