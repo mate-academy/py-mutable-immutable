92bc69070d3cd98ce88e4380d24c9ed15d07638b
Denys <djkjibyltybc@gmail.com>
2025-01-28 19:55:23 +0200

    sorting into mutable and immutable objects

--- app/main.py
+++ app/main.py
@@ -16,4 +16,17 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+def sort_variables(list_object: list) -> dict:
+    my_sorted_variables = {"mutable": [], "immutable": []}
+    for variable_object in list_object:
+        if isinstance(variable_object, (int, str, bool, float, tuple)):
+            my_sorted_variables["immutable"].append(variable_object)
+        else:
+            my_sorted_variables["mutable"].append(variable_object)
+    return my_sorted_variables
+
+
+my_object = [lucky_number, pi, one_is_a_prime_number, name,
+             my_favourite_films, profile_info, marks, collection_of_coins]
+sorted_variables = sort_variables(my_object)
