8d495aea01e92507005d493816a9caad3daac6b2
Alex <malynovskijalex23@gmail.com>
2025-02-11 14:55:38 +0200

    Добавлены 8 переменных и исправлен словарь sorted_variables

--- app/main.py
+++ app/main.py
@@ -16,4 +16,24 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Определение переменных разных типов
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, World!"
+bool_var = True
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+set_var = {7, 8, 9}
+dict_var = {"key": "value"}
+
+# Создание словаря sorted_variables
+sorted_variables = {
+    "numbers": [integer_var, float_var],
+    "sequences": [string_var, list_var, tuple_var],
+    "boolean": [bool_var],
+    "collections": [set_var, dict_var]
+}
+
+# Вывод результата
+print(sorted_variables)
+
