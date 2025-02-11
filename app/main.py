3387f5503669134a051dc79b966543852ad585de
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-02-11 14:10:57 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,10 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Create the dictionary with the classification of variables
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
+
+print(sorted_variables)
