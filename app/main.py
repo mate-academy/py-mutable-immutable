b4f1e36f67a62d3bd00dfaa7ae86b1b0f16cd856
Alex <malynovskijalex23@gmail.com>
2025-02-12 16:28:48 +0200

    Добавил примеры изменяемых и неизменяемых типов

--- app/main.py
+++ app/main.py
@@ -1,38 +1,14 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+immutable_int = 10
+immutable_str = "Hello"
+immutable_tuple = (1, 2, 3)
 
-# Определение переменных разных типов
-integer_var = 42
-float_var = 3.14
-string_var = "Hello, World!"
-bool_var = True
-list_var = [1, 2, 3]
-tuple_var = (4, 5, 6)
-set_var = {7, 8, 9}
-dict_var = {"key": "value"}
+immutable_int += 1
+immutable_str += " World"
 
-# Создание словаря sorted_variables
-sorted_variables = {
-    "numbers": [integer_var, float_var],
-    "sequences": [string_var, list_var, tuple_var],
-    "boolean": [bool_var],
-    "collections": [set_var, dict_var]
-}
+mutable_list = [1, 2, 3]
+mutable_dict = {"a": 1, "b": 2}
+mutable_set = {1, 2, 3}
 
-# Вывод результата
-print(sorted_variables)
+mutable_list.append(4)
+mutable_dict["c"] = 3
+mutable_set.add(4)
