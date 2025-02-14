7fbabeea534de848fbcdea9a6e70535edb12b0ed
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-02-14 13:49:58 +0100

    feat: sort variables with comments immutable, mutable

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+# immutable data types
 lucky_number: int = 777
 pi: float = 3.14
 one_is_a_prime_number: bool = False
 name: str = "Richard"
+profile_info: tuple = ("michel", "michel@gmail.com", "12345678")
+
+# mutable data types
 my_favourite_films: list = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
@@ -9,11 +13,9 @@ my_favourite_films: list = [
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-profile_info: tuple = ("michel", "michel@gmail.com", "12345678")
 marks: dict = {
     "John": 4,
     "Sergio": 3,
 }
 collection_of_coins: set = {1, 2, 25}
 
-# write your code here
