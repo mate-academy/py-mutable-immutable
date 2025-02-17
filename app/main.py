ff497548880b61e0ba6a6296eaa7d8b4938624d1
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2025-02-17 23:06:07 +0200

    fix lint

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ collection_of_coins = {1, 2, 25}
 
 
 sorted_variables = {
-    "mutable": [my_favourite_films, marks],
+    "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number,
-                  name, profile_info, collection_of_coins]
+                  name, profile_info]
 }
