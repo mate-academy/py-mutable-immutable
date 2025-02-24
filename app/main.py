a9a3dc97cec82bed49a80b5efb7f6312b2050792
Artem Herasymchuk <artem.herasymchuk@gmail.com>
2025-02-24 20:16:28 +0100

    Lösung2

--- app/main.py
+++ app/main.py
@@ -1,25 +1,27 @@
-# Given 8 variables of different data types
-a = 123  # Immutable (int)
-b = []  # Mutable (list)
-c = "Hi!"  # Immutable (str)
-d = [1, 2]  # Mutable (list)
-e = (4, 5)  # Immutable (tuple)
-f = {6, 7}  # Mutable (set)
-g = {"key": "value"}  # Mutable (dict)
-h = 3.14  # Immutable (float)
-
+integer_value = 123
+empty_list = []
+greeting_text = "Hi!"
+number_list = [1, 2]
+coordinates_tuple = (4, 5)
+unique_numbers_set = {6, 7}
+data_dictionary = {"key": "value"}
+pi_value = 3.14
 
 mutable_list = []
 immutable_list = []
 
 # Classifying variables
-for var in [a, b, c, d, e, f, g, h]:
-    if isinstance(var, (list, dict, set)):  # Mutable types
-        mutable_list.append(var)
-    else:  # Immutable types
-        immutable_list.append(var)
+variables = [
+    integer_value, empty_list, greeting_text, number_list,
+    coordinates_tuple, unique_numbers_set, data_dictionary, pi_value
+]
 
-# Creating the dictionary
+for item in variables:
+    if isinstance(item, (list, dict, set)):
+        mutable_list.append(item)
+    else:
+        immutable_list.append(item)
+
 sorted_variables = {
     "mutable": mutable_list,
     "immutable": immutable_list
