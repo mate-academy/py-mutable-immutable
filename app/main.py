0266224a8c2e60d2d253f99cdc7e16085f9b6c09
Artem Herasymchuk <artem.herasymchuk@gmail.com>
2025-02-24 19:58:54 +0100

    Lösung

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+# Given 8 variables of different data types
+a = 123  # Immutable (int)
+b = []  # Mutable (list)
+c = "Hi!"  # Immutable (str)
+d = [1, 2]  # Mutable (list)
+e = (4, 5)  # Immutable (tuple)
+f = {6, 7}  # Mutable (set)
+g = {"key": "value"}  # Mutable (dict)
+h = 3.14  # Immutable (float)
 
+
+mutable_list = []
+immutable_list = []
+
+# Classifying variables
+for var in [a, b, c, d, e, f, g, h]:
+    if isinstance(var, (list, dict, set)):  # Mutable types
+        mutable_list.append(var)
+    else:  # Immutable types
+        immutable_list.append(var)
+
+# Creating the dictionary
+sorted_variables = {
+    "mutable": mutable_list,
+    "immutable": immutable_list
+}
+
+print(sorted_variables)
