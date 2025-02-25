483d77529e3ce141b62ac70f4103e8e79a21f93b
Artem Herasymchuk <artem.herasymchuk@gmail.com>
2025-02-25 19:05:12 +0100

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -1,27 +1,39 @@
-integer_value = 123
-empty_list = []
-greeting_text = "Hi!"
-number_list = [1, 2]
-coordinates_tuple = (4, 5)
-unique_numbers_set = {6, 7}
-data_dictionary = {"key": "value"}
-pi_value = 3.14
+lucky_number = 777  # Immutable (int)
+pi = 3.14  # Immutable (float)
+one_is_a_prime_number = False  # Immutable (bool)
+name = "Richard"  # Immutable (str)
+my_favourite_films = (
+    "Inception", "The Matrix", "Interstellar"
+)  # Immutable (tuple)
+profile_info = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}  # Mutable (dict)
+marks = {
+    "John": 4,
+    "Alice": 5,
+    "Bob": 3
+}
+collection_of_coins = {1, 2, 5, 10, 20, 50}  # Immutable (frozenset)
 
 mutable_list = []
 immutable_list = []
 
-# Classifying variables
 variables = [
-    integer_value, empty_list, greeting_text, number_list,
-    coordinates_tuple, unique_numbers_set, data_dictionary, pi_value
+    lucky_number, pi, one_is_a_prime_number, name,
+    my_favourite_films, profile_info, marks, collection_of_coins
 ]
+
 for item in variables:
     if isinstance(item, (list, dict, set)):
         mutable_list.append(item)
     else:
         immutable_list.append(item)
+
 sorted_variables = {
     "mutable": mutable_list,
     "immutable": immutable_list
 }
+
 print(sorted_variables)
