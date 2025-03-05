61d84732df1f46da0f46a2f2415a6991b46b37ee
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2025-03-06 00:29:33 +0100

    2 Solutions for task

--- app/main.py
+++ app/main.py
@@ -16,4 +16,29 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# sorted_variables = {'mutable': [], 'immutable': []}
+#
+# def mutable_immutable(*var) -> dict:
+#     ls_mutable = []
+#     ls_immutable = []
+#     for i in range(len(var)):
+#         for name, val in globals().items():
+#             if val == var[i]:
+#                 break
+#         if type(val) == list or type(val) == set or type(val) == dict:
+#             sorted_variables['mutable'].append(name)
+#         else:
+#             sorted_variables['immutable'].append(name)
+#
+#     # sorted_variables['mutable'] = ls_mutable
+#     # sorted_variables['immutable'] = ls_immutable
+#
+#     return sorted_variables
+#
+#
+# print(mutable_immutable(lucky_number, pi, one_is_a_prime_number, name, my_favourite_films, profile_info, marks, collection_of_coins))
+
+sorted_variables = {
+    'mutable': [my_favourite_films, marks, collection_of_coins],
+    'immutable': [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
