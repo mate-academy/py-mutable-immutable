657aee3602062b7fc4adbe61f92e0c210cfe7e51
Yevgene Kuznietsov <gluman@meta.ua>
2025-03-08 16:58:04 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,12 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# create uk_key
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
+
+# Перевірка результату
+print("Mutable:", sorted_variables["mutable"])
+print("Immutable:", sorted_variables["immutable"])
