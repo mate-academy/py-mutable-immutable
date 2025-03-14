6549bbb2724fd7080c2e39277eda851eeb1f6275
Illya Aleksandrov <illyaalexandrov@gmail.com>
2025-03-14 13:42:50 +0100

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -16,10 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# Categorizing variables
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
-# Output the result
-print(sorted_variables)
