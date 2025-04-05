a58bb08208afc8f1801a8c3d4ac294f5b8810e13
Yura Valko <yuravalko166@gmail.com>
2025-04-05 15:34:44 +0300

    add interpreter

--- app/main.py
+++ app/main.py
@@ -2,18 +2,14 @@ lucky_number :int = 777
 pi :float = 3.14
 one_is_a_prime_number :bool = False
 name :str = "Richard"
-my_favourite_films = [
+my_favourite_films :list = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
+    "The Matrix",]
 profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+marks : dict= {"John": 4,"Sergio": 3,}
+collection_of_coins :set = {1, 2, 25}
 
 # write your code here
