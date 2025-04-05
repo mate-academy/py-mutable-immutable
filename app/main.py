7874d14f8f4fe86e658aaf0775f18d49b420e222
Yura Valko <yuravalko166@gmail.com>
2025-04-05 15:47:32 +0300

    add last line

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ collection_of_coins :set = {1, 2, 25}
 # write your code here
 sorted_variables = {"mutable" :[my_favourite_films, marks, collection_of_coins],
                     "immutable" :[lucky_number, pi, one_is_a_prime_number, name, profile_info, ]
-                    }
+                   }
