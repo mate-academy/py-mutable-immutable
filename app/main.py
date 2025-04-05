a52851d95e4f0e8e4fe3369577d1a47abafc64d7
Yura Valko <yuravalko166@gmail.com>
2025-04-05 15:50:38 +0300

    fix problem

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-lucky_number :int = 777
-pi :float = 3.14
-one_is_a_prime_number :bool = False
-name :str = "Richard"
-my_favourite_films :list = [
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
+my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",]
 profile_info = ("michel", "michel@gmail.com", "12345678")
-marks : dict= {"John": 4,"Sergio": 3,}
-collection_of_coins :set = {1, 2, 25}
+marks = {"John": 4,"Sergio": 3,}
+collection_of_coins  = {1, 2, 25}
 
 # write your code here
 sorted_variables = {"mutable" :[my_favourite_films, marks, collection_of_coins],
