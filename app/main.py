e427ff92c464d2e0e1b72ac697dc728dd0618e85
Yura Valko <yuravalko166@gmail.com>
2025-04-05 15:41:39 +0300

    Implemented

--- app/main.py
+++ app/main.py
@@ -13,3 +13,6 @@ marks : dict= {"John": 4,"Sergio": 3,}
 collection_of_coins :set = {1, 2, 25}
 
 # write your code here
+sorted_variables = {"mutable" :[my_favourite_films, marks, collection_of_coins],
+                    "immutable" :[lucky_number, pi, one_is_a_prime_number, name, profile_info, ]
+                    }
