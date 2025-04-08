fac78956c8a3531b48340a07d5cd7719f96ed1f6
Vitalii Prushak <v.prushak@gmail.com>
2025-04-08 22:55:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,34 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+
+def sort_variables(*args) -> dict:
+    sorted_variables = {
+        "mutable": [],
+        "immutable": []
+    }
+
+    mutable_types = (list, dict, set, bytearray)
+    immutable_types = (int, float, str, tuple, bool, bytes)
+
+    for arg in args:
+        if isinstance(arg, mutable_types):
+            sorted_variables["mutable"].append(arg)
+        elif isinstance(arg, immutable_types):
+            sorted_variables["immutable"].append(arg)
+        else:
+            pass
+
+    return sorted_variables
+
+
+sorted_variables = sort_variables(
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+)
