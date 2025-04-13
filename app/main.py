8629369b246348fa1991d6354ecb754a61a5212e
Aidenak <aidenakamori@gmail.com>
2025-04-13 13:04:42 +0200

    Add sorted_variables dictionary for mutable/immutable task

--- app/main.py
+++ app/main.py
@@ -17,3 +17,18 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # write your code here
+# main.py
+
+a = 123                # int - immutable
+b = [1, 2, 3]          # list - mutable
+c = "Hi!"              # str - immutable
+d = {"name": "Aiden"}  # dict - mutable
+e = (5, 6, 7)          # tuple - immutable
+f = 3.14               # float - immutable
+g = {1, 2, 3}          # set - mutable
+h = False              # bool - immutable
+
+sorted_variables = {
+    "mutable": [b, d, g],
+    "immutable": [a, c, e, f, h]
+}
