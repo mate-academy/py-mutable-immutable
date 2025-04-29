2c37b5aab014b54fb71c50b8e8dff91090b4c9d4
Aronatto <koksik122@o2.pl>
2025-04-29 13:44:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
-# Zmienne niemutowalne (immutable)
 lucky_number = 777               # int – liczby całkowite są niemutowalne
 pi = 3.14                        # float – liczby zmiennoprzecinkowe są niemutowalne
 one_is_a_prime_number = False   # bool – wartości logiczne są niemutowalne
 name = "Richard"                # str – ciągi znaków są niemutowalne
-profile_info = ("michel", "michel@gmail.com", "12345678")  # tuple – krotki są niemutowalne
-
+profile_info = (
+    "michel",
+    "michel@gmail.com",
+    "12345678",  # tuple – tuples are immutable
+)
 # Zmienne mutowalne (mutable)
-my_favourite_films = [          # list – listy można zmieniać (dodawać, usuwać elementy)
+my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-marks = {                       # dict – słowniki można zmieniać (dodawać, usuwać pary klucz-wartość)
+marks = {
     "John": 4,
     "Sergio": 3,
 }
@@ -33,12 +35,6 @@ sorted_variables = {
         profile_info
     ]
 }
-
-
-
-
-
-
 
 
 
