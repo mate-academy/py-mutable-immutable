f34749cf0e34f4ecc3faaa291305ff028a092f42
Jakub <jakubmichalski89@fgmail.com>
2025-04-29 14:18:01 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,4 +16,11 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Tworzenie słownika z podziałem na typy
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
+}
+
+# Wyświetlenie wyniku (opcjonalne)
+print(sorted_variables)
