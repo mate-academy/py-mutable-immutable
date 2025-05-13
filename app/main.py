247f9cf0a483288fde501a2fb479f27125708de7
Nicolas-Jacob <nicolasoliveirajacob@gmail.com>
2025-05-13 19:47:23 -0300

    Mutables and Immutables added

--- app/main.py
+++ app/main.py
@@ -17,3 +17,18 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # write your code here
+
+a_list = [1, 2, 3]
+a_dict = {'x': 1, 'y': 2}
+a_set = {1, 2, 3}
+
+a_int = 42
+a_str = "hello"
+a_tuple = (1, 2, 3)
+a_float = 3.14
+
+# Categorizing variables
+sorted_variables = {
+    "mutable": [a_list, a_dict, a_set],
+    "immutable": [a_int, a_str, a_tuple, a_float]
+}
