0b17524b45695a32c86f27b6ce917df68d08e599
Daniel Karnopp Schuh <danielkchuh@gmail.com>
2025-05-14 11:36:05 -0300

    Resolved collection_of_coins is mutable

--- app/main.py
+++ app/main.py
@@ -17,7 +17,9 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 # collection_of_coins[1] = 10
-# TypeError: 'set' object does not support item assignment
+# TypeError: 'set' object does not support item assignment is immutable
+# or
+# collection_of_coins.add(4) is mutable
 
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
