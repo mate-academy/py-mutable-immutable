2dad532ffae5236f82c46ada1267a751034abf57
Mykhailo Vasyliev <marchinglobster@gmail.com>
2025-05-31 14:33:44 +0300

    fixed dict style

--- app/main.py
+++ app/main.py
@@ -19,5 +19,4 @@ collection_of_coins = {1, 2, 25}
 sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
-
 }
