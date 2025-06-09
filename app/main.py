e258160e03c1d905e4304982ad0119194eb8e7e1
Josias <josiasonesokataborda22@gmail.com>
2025-06-09 15:44:58 -0300

    Variables separated into mutable and immutable

--- app/main.py
+++ app/main.py
@@ -16,4 +16,9 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_variables = {
+    "mutable":[my_favourite_films, marks, collection_of_coins],
+    "immutable":[lucky_number, pi, name, profile_info, one_is_a_prime_number]
+}
+
+print(sorted_variables)
