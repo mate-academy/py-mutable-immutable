a69ca09e031fc6ca9ff3222d7a8416abc3048de2
yarik <super.racing@ukr.net>
2025-06-18 01:45:23 +0300

    mistake was corrected

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {}
-sorted_variables.update({"mutable": [my_favourite_films, marks], "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]})
+sorted_variables.update({"mutable": [my_favourite_films, marks, collection_of_coins], "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]})
