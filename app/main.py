f89edc6889e5dc9f00d6266c5dabe04afb56667c
Oleksiy Kotov <kotov.oleksiy@icloud.com>
2025-07-02 21:50:30 +0300

    sorted mutable and immutable

--- app/main.py
+++ app/main.py
@@ -18,5 +18,5 @@ collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
 "mutable": [my_favourite_films, marks, collection_of_coins],
-"immutable":[lucky_number, pi, one_is_a_prime_number, name, profile_info]
+"immutable":[lucky_number, pi,name, one_is_a_prime_number, profile_info]
 }
