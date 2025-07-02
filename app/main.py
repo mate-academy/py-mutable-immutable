71252af017d35969995fe42647b3e5478d522963
Oleksiy Kotov <kotov.oleksiy@icloud.com>
2025-07-02 21:30:25 +0300

    sorted mutable and immutable
    # Conflicts:
    #	app/main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,22 @@
-from attr.validators import instance_of
-
-
-
-lucky_number = 777 #int
-pi = 3.14 #float
-one_is_a_prime_number = False #boole
-name = "Richard" #str
+lucky_number = 777
+pi = 3.14
+one_is_a_prime_number = False
+name = "Richard"
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
-] #list
-profile_info = ("michel", "michel@gmail.com", "12345678") #tuple
+]
+profile_info = ("michel", "michel@gmail.com", "12345678")
 marks = {
     "John": 4,
     "Sergio": 3,
-} #dict
-collection_of_coins = {1, 2, 25}  #set
+}
+collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [my_favourite_films, marks, collection_of_coins],
-    "immutable": [lucky_number, pi, name, one_is_a_prime_number, profile_info]
+"mutable": [my_favourite_films, marks, collection_of_coins],
+"immutable":[lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
-
-print("Loading app.main...")
