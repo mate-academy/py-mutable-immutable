6c874a3947872f1774825c7d1142923d5cc94e02
vetal <semeniv09@ukr.net>
2025-07-21 17:08:29 +0000

    Fix: remove HEAD and duplicates, pass flake8

--- app/main.py
+++ app/main.py
@@ -24,21 +24,27 @@ sorted_variables = {
 }
 
 
-
 lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
 name = "Richard"
 my_favourite_films = [
     "The Shawshank Redemption",
-    "The Lord of the Rings",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
 ]
-profile = {"name": "John", "age": 30}
-unique_ids = {1001, 1002, 1003}
-coordinates = (50.4501, 30.5234)
+profile_info = ("michel", "michel@gmail.com", "12345678")
+marks = {
+    "John": 4,
+    "Sergio": 3,
+}
+collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [my_favourite_films, profile, unique_ids],
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, coordinates]
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
+
 
