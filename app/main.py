2dbaaabe494eb2be881fe6b505a1adf76687e8ef
Luan <luan.valentino78@gmail.com>
2025-08-12 15:56:10 -0300

    task solved

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
+lucky_number = 777         # int (inteiro)
+pi = 3.14                  # float (número com ponto flutuante)
+one_is_a_prime_number = False # bool (booleano)
+name = "Richard"           # str (string)
+my_favourite_films = [     # list (lista)
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
+profile_info = ("michel", "michel@gmail.com", "12345678") # tuple (tupla)
+marks = {                  # dict (dicionário)
     "John": 4,
     "Sergio": 3,
 }
-collection_of_coins = {1, 2, 25}
+collection_of_coins = {1, 2, 25} # set (conjunto)
 
-# write your code here
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
+
