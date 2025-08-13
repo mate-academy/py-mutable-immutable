df6edf59e425fea27486fd3c4a0b8a92f3fb6ac4
Danylo Tatarchuk <tatarchuk.danil06@gmail.com>
2025-08-13 19:08:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,37 @@
-lucky_number = 777
-pi = 3.14
-one_is_a_prime_number = False
-name = "Richard"
+# immutable
+lucky_number = 777  # int
+pi = 3.14  # float
+one_is_a_prime_number = False  # bool
+name = "Richard"  # str
+profile_info = ("michel", "michel@gmail.com", "12345678")  # tuple
+
+
+# mutable
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
+]  # list
+
 marks = {
     "John": 4,
     "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+}  # dict
+collection_of_coins = {1, 2, 25}   # set
 
-# write your code here
+sorted_variables = {
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info
+    ],
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins
+    ]
+}
