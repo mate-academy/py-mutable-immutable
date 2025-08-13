a381a218d84fdc686f5e984e5181a31b031f9cfb
Yuriy Sobakar <raizen.dj@gmail.com>
2025-08-13 12:17:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,33 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+def sort_varibles(*args) -> dict:
+    sorted_variables = {}
+    mutable_types = (list, dict, set)
+
+    for var in args:
+        if type(var) in mutable_types:
+            if 'mutable' not in sorted_variables:
+                sorted_variables['mutable'] = [var]
+            else:
+                sorted_variables['mutable'].append(var)
+        else:
+            if 'immutable' not in sorted_variables:
+                sorted_variables['immutable'] = [var]
+            else:
+                sorted_variables['immutable'].append(var)
+
+    return sorted_variables
+
+sorted_variables = sort_varibles(
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins
+)
+
+print(sorted_variables)
