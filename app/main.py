ce058c306ae47d6a9dbd94c21ff74fc1866b9f0b
Serhii Titovets <sergtitos@gmail.com>
2025-08-18 16:03:32 +0300

    Solution_fix

--- app/main.py
+++ app/main.py
@@ -15,7 +15,21 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-sorted_variables = {
-    "mutable": [my_favourite_films, marks, collection_of_coins],
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+all_vars = {
+    name: value
+    for name, value in globals().items()
+    if not name.startswith('_') and name != 'sorted_variables'
 }
+
+# Разделение на mutable и immutable
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+
+for var_name, var in all_vars.items():
+    if isinstance(var, (list, dict, set)):  # Mutable-типы
+        sorted_variables["mutable"].append(var_name)
+    elif isinstance(var, (int, float, str, bool, tuple)):  # Immutable-типы
+        sorted_variables["immutable"].append(var_name)
+print(sorted_variables)
