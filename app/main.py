2a70f744429315dc4f3826cc0f874f47dbd33e53
Serhii Titovets <sergtitos@gmail.com>
2025-08-19 14:00:29 +0300

    Solution_1

--- app/main.py
+++ app/main.py
@@ -16,4 +16,20 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+all_vars = {
+    name: value
+    for name, value in globals().items()
+    if not name.startswith('_') and name != 'sorted_variables'
+}
+
+# Разделение на mutable иimmutable
+sorted_variables = {
+    "mutable": [],
+    "immutable": []
+}
+
+for var_name, var in all_vars.items():
+    if isinstance(var, (list, dict, set)):  # Mutable-типы
+        sorted_variables["mutable"].append(var)
+    elif isinstance(var, (int, float, str, bool, tuple)):  # Immutable-типы
+        sorted_variables["immutable"].append(var)
