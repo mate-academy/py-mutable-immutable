1078eac0a8b9835bb77dc3ad92bf51e1bb55eee4
Trust3-hub <trustgarnnetmpofu@gmail.com>
2025-08-20 12:04:39 +0200

    Update py_mutable_immutable.py

--- py_mutable_immutable.py
+++ py_mutable_immutable.py
@@ -5,7 +5,7 @@ d = 3.14 # immutable
 e = {2, 4, 6} # mutable
 f ={"tg": 1, "tr": "good"}  # mutable
 g = ("a", "b", "c") # immutable
-h = bytearray(c"Hello")  # mutable
+h = bytearray(b"Hello")  # mutable
 
 sorted_variables = {
     "mutable": [b, e, f, h],
