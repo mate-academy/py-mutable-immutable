ce480cca42b2b5448ee20843bf409fe866ee519e
Daniel Joseph <djoseph@envisagebr.com>
2025-09-17 10:27:09 +0300

    fix: Atualiza o valor de my_favourite_films

--- app/main.py
+++ app/main.py
@@ -10,7 +10,13 @@ profile_info = ("michel", "michel@gmail.com", "12345678")
 
 # Tipos Mutáveis
 # ESTA É A LISTA CORRETA QUE O TESTE ESPERA:
-my_favourite_films = ["Inception", "The Matrix", "Interstellar"]
+my_favourite_films = [
+    "The Shawshank Redemption",
+    "The Lord of the Rings: The Return of the King",
+    "Pulp Fiction",
+    "The Good, the Bad and the Ugly",
+    "The Matrix",
+]
 marks = {"John": 4, "Sergio": 3}
 collection_of_coins = {1, 2, 25}
 
