cb81017268200be5fac48d8f64deca17594d62f3
Jailson J Martins <Jailson.Martins@eris.cv>
2025-09-25 16:14:53 -0100

    Solution

--- app/main.py
+++ app/main.py
@@ -11,22 +11,10 @@ my_favourite_films = [
 ]
 profile_info = ("michel", "michel@gmail.com", "12345678")
 marks = {
-    "John": 9,
-    "Sergio": 11,
+    "John": 4,
+    "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
-# write your code here
-
-# The 8 variables of different data types
-int_var = 1
-float_var = 2.5
-str_var = "hello"
-bool_var = True
-list_var = [1, 2, 3]
-tuple_var = (4, 5, 6)
-set_var = {7, 8, 9}
-dict_var = {"key": "value"}
 
 # Create the sorted_variables dictionary
 sorted_variables = {
@@ -36,16 +24,17 @@ sorted_variables = {
 
 # List of all variables
 all_variables = [
-    int_var, float_var, str_var, bool_var,
-    list_var, tuple_var, set_var, dict_var
+    lucky_number, pi, one_is_a_prime_number, name,
+    my_favourite_films, profile_info, marks, collection_of_coins
 ]
 
+
 # Iterate through the variables and sort them
-for var in all_variables:
-    if isinstance(var, (list, dict, set)):
-        sorted_variables["mutable"].append(var)
+for variable in all_variables:
+    if isinstance(variable, (list, dict, set)):
+        sorted_variables["mutable"].append(variable)
     else:
-        sorted_variables["immutable"].append(var)
+        sorted_variables["immutable"].append(variable)
 
 # Print the resulting dictionary to verify
 print(sorted_variables)
