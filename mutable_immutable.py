36c1832a5face99ca78dbec31539c17ad8b443f5
Alla Naumenko <naumenko.alla@gmail.com>
2025-09-25 11:46:40 +0800

    Update mutable_immutable.py

--- mutable_immutable.py
+++ mutable_immutable.py
@@ -1,13 +1,13 @@
-a = 123          # int 
-b = []           # list 
-c = "Hi!"        # str 
-d = [1, 2]       # list 
-e = 3.14         # float 
-f = (1, 2, 3)    # tuple 
-g = {"key": "value"}  # dict 
-h = {1, 2, 3}    # set 
+lucky_number = 678                # int 
+pi = 3.14                          # float 
+one_is_a_prime_number = False      # bool 
+name = "Alla"                      # str 
+my_favourite_films = ["Some Like It Hot", "The Godfather"]  # list 
+profile_info = {"age": 42, "city": "Kyiv"}    # dict 
+marks = (5, 4, 3)                  # tuple 
+collection_of_coins = {1, 2, 25}   # set 
 
 sorted_variables = {
-    "mutable": [b, d, g, h],
-    "immutable": [a, c, e, f]}
+    "mutable": [my_favourite_films, profile_info, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, marks]}
 
