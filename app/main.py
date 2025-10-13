b4c23268e044c583d73c9df4a699ccf548d189ae
Ihor <ihor.hrymut@gmail.com>
2025-10-13 12:28:51 +0200

    My solution py-mutable-immutable

--- app/main.py
+++ app/main.py
@@ -14,6 +14,33 @@ marks = {
     "John": 4,
     "Sergio": 3,
 }
+data_storage = {
+    "lucky_number": 777,
+    "pi": 3.14,
+    "one_is_a_prime_number": False,
+    "name": "Richard",
+    "my_favourite_films": [
+        "The Shawshank Redemption",
+        "The Lord of the Rings: The Return of the King",
+        "Pulp Fiction", "The Good, the Bad and the Ugly",
+        "The Matrix"
+    ],
+    "profile_info": ("michel", "michel@gmail.com", "12345678"),
+    "marks": {"John": 4, "Sergio": 3},
+    "collection_of_coins": {1, 2, 25},
+}
 collection_of_coins = {1, 2, 25}
+mutable = []
+immutable = []
+
+for value in data_storage.values():
+    if isinstance(value, (list, dict, set)):
+        mutable.append(value)
+    else:
+        immutable.append(value)
 
 # write your code here
+sorted_variables = {
+    "mutable": mutable,
+    "immutable": immutable
+}
