277fbc3629cc57b955ef6e45458620b62f6b47d7
Ostap Shp <stark171705@gmail.com>
2025-10-16 16:53:44 +0300

    completed task

--- app/main.py
+++ app/main.py
@@ -16,4 +16,22 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+"""
+There are 8 variables of different data types in the main module.
+Your task is to create dictionary sorted_variables with two keys: "mutable" and "immutable".
+Each value should be equal to a list that contains all variables of corresponding type.
+Example with other variables:
+a = 123
+b = []
+c = "Hi!"
+d = [1, 2]
+sorted_variables = {
+    "mutable": [b, d],
+    "immutable": [a, c]
+}
+"""
+
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
