eaedf2cee837f71575577ab761ec86816a0cf1a4
Thiago Barbosa <thiago18cg_rj@hotmail.com>
2025-10-16 18:45:56 -0300

    Solution create dictionary

--- app/create_dictionary.py
+++ app/create_dictionary.py
@@ -1,12 +1,12 @@
 def create_dictionary(*args):
     valid_types = (int, float, str, bool, type(None), tuple)
-    d = {}
+    dic = {}
 
     for index, arg in enumerate(args):
         # Verifica se é função (callable) ou tipo permitido e que seja *hashável*
         if isinstance(arg, valid_types) or callable(arg):
-            d[arg] = index
+            dic[arg] = index
         else:
             print(f"Cannot add {arg} to the dict!")
 
-    return d
+    return dic
