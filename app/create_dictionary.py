88d43ac35f13fa45665b0ce25f835edfa84ad69e
Thiago Barbosa <thiago18cg_rj@hotmail.com>
2025-10-17 19:13:10 -0300

    create dictionary

--- app/create_dictionary.py
+++ app/create_dictionary.py
@@ -1,7 +1,6 @@
 def create_dictionary(*args):
     dic = {}
     for index, arg in enumerate(args):
-        # Aceita apenas tipos hasháveis especificados ou callables (funções)
         if isinstance(arg, (int, float, str, bool, type(None), tuple)) or callable(arg):
             dic[arg] = index
         else:
