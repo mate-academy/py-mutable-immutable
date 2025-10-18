416601371f50df81d5ee408a486559352b2e1722
Farlinder <sashakost2006e@gmail.com>
2025-10-18 18:51:41 +0200

    Del print string

--- app/main.py
+++ app/main.py
@@ -26,6 +26,3 @@ sorted_variables = {
     "mutable": [my_favourite_films, marks, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
 }
-
-if __name__ == "__main__":
-    print(sorted_variables)
