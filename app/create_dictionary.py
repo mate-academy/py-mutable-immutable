f4c352e04d6e08cc636e6f5dac1e5e76135c8584
Thiago Barbosa <thiago18cg_rj@hotmail.com>
2025-10-20 19:37:28 -0300

    Create dictionary

--- app/create_dictionary.py
+++ app/create_dictionary.py
@@ -3,7 +3,7 @@ def create_dictionary(*args) -> int:
     for index, arg in enumerate(args):
         if (isinstance(arg, (int, float, str, bool, type(None), tuple))
             or callable(arg)):
-            dic[arg] = index
+                dic[arg] = index
         else:
             print(f"Cannot add {arg} to the dict!")
     return dic
