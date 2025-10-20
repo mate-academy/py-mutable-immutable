b1a4223744887332fe619f6e6dbc78097f624a27
Thiago Barbosa <thiago18cg_rj@hotmail.com>
2025-10-20 19:49:41 -0300

    Create dictionary

--- app/create_dictionary.py
+++ app/create_dictionary.py
@@ -1,9 +1,11 @@
 from typing import Any, Dict
 
+
 def create_dictionary(*args) -> Dict[Any, int]:
     dic: Dict[Any, int] = {}
     for index, arg in enumerate(args):
-        if isinstance(arg, (int, float, str, bool, type(None), tuple)) or callable(arg):
+        if (isinstance(arg,(int,float,str,bool,type(None),tuple))
+                or callable(arg)):
             try:
                 dic[arg] = index
             except TypeError:
