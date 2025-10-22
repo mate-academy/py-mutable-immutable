6da16210376098bd2c64d3f224db2b69656a6477
Denys Dumanetskyi <dumaneckijdenis@gmail.com>
2025-10-22 14:03:50 +0300

    I fixed my mistake in variable name

--- app/main.py
+++ app/main.py
@@ -18,5 +18,5 @@ collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
     "mutable": [my_favourite_films, collection_of_coins, marks],
-    "imutable": [name, one_is_a_prime_number, pi, lucky_number, profile_info]
+    "immutable": [name, one_is_a_prime_number, pi, lucky_number, profile_info]
 }
