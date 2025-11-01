37ff2e101f858982c515ee505609cf88b041f273
Artem Kurhan <artemkurhan341@gmail.com>
2025-11-01 20:00:55 +0100

    Fixed mistake

--- app/main.py
+++ app/main.py
@@ -40,3 +40,6 @@ sorted_variables = to_sort_variables({
     "marks": marks,
     "collection_of_coins": collection_of_coins
 })
+
+print(sorted_variables)
+sorted_variables = {'mutable': ['my_favourite_films', 'marks', 'collection_of_coins'], 'immutable': ['lucky_number', 'pi', 'one_is_a_prime_number', 'name', 'profile_info']}
