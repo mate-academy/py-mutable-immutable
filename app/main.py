80de58797549c0ee1e259d399c1d7a00c213562e
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2025-11-10 19:53:51 +0200

    Fix flake8 style errors in main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ lucky_number = 777
 pi = 3.14
 one_is_a_prime_number = False
 name = "Richard"
+
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
@@ -9,19 +10,28 @@ my_favourite_films = [
     "The Good, the Bad and the Ugly",
     "The Matrix",
 ]
+
 profile_info = ("michel", "michel@gmail.com", "12345678")
+
 marks = {
     "John": 4,
     "Sergio": 3,
 }
+
 collection_of_coins = {1, 2, 25}
 
-def sorted_variables(*args):
-    d = {}
+
+def sorted_variables(*args) -> dict:
+    """Повертає словник, де ключ — значення змінної, а значення — її індекс."""
+    variables_dict = {}
+
     for index, arg in enumerate(args):
-        if isinstance(arg, (int, float, str, bool, tuple, type(None))) or callable(arg):
-            d[arg] = index
+        if isinstance(arg, (int,  str, bool, tuple, type(None))) or callable(arg):
+            variables_dict[arg] = index
         else:
             print(f"Cannot add {arg} to the dict!")
-    return d
-print(sorted_variables(lucky_number,pi,one_is_a_prime_number,name,my_favourite_films))
+
+    return variables_dict
+
+
+print(sorted_variables(lucky_number, pi, one_is_a_prime_number, name, my_favourite_films))
