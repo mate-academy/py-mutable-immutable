972702ade7c84d5e18aa82ab47af8b6f13a37a51
Tanya210300 <gonchartanya9@gmail.com>
2025-11-11 14:17:56 +0200

    problen solution mutable/immutable

--- app/main.py
+++ app/main.py
@@ -16,4 +16,38 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+sorted_values = {"mutable":[], "immutable":[]}
+if isinstance(lucky_number,(list,dict,set)):
+    sorted_values["mutable"].append("lucky_number")
+else:
+    sorted_values["immutable"].append("lucky_number")
+if isinstance(pi,(list,dict,set)):
+    sorted_values["mutable"].append("pi")
+else:
+    sorted_values["immutable"].append("pi")   
+if isinstance(one_is_a_prime_number,(list,dict,set)):
+    sorted_values["mutable"].append("one_is_a_prime_number")
+else:
+    sorted_values["immutable"].append("one_is_a_prime_number")
+if isinstance(name,(list,dict,set)):
+    sorted_values["mutable"].append("name")
+else:
+    sorted_values["immutable"].append("name")
+if isinstance(my_favourite_films,(list,dict,set)):
+    sorted_values["mutable"].append("my_favourite_films")
+else:
+    sorted_values["immutable"].append("my_favourite_films")
+if isinstance(profile_info,(list,dict,set)):
+    sorted_values["mutable"].append("profile_info")
+else:
+    sorted_values["immutable"].append("profile_info")
+if isinstance(marks,(list,dict,set)):
+    sorted_values["mutable"].append("marks")
+else:
+    sorted_values["immutable"].append("marks")
+if isinstance(collection_of_coins,(list,dict,set)):
+    sorted_values["mutable"].append("collection_of_coins")
+else:
+    sorted_values["immutable"].append("collection_of_coins")
+print(sorted_values)
+
