6fca43a50dc7c79c3a13bc26277408710a162603
Tanya210300 <gonchartanya9@gmail.com>
2025-11-11 20:46:28 +0200

    correct 3

--- app/main.py
+++ app/main.py
@@ -15,40 +15,12 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-
-sorted_variables = {"mutable":[], "immutable":[]}
-if isinstance(lucky_number,(list,dict,set)):
-    sorted_variables["mutable"].append(lucky_number)
-else:
-    sorted_variables["immutable"].append(lucky_number)
-if isinstance(pi,(list,dict,set)):
-    sorted_variables["mutable"].append(pi)
-else:
-    sorted_variables["immutable"].append(pi)   
-if isinstance(one_is_a_prime_number,(list,dict,set)):
-    sorted_variables["mutable"].append(one_is_a_prime_number)
-else:
-    sorted_variables["immutable"].append(one_is_a_prime_number)
-if isinstance(name,(list,dict,set)):
-    sorted_variables["mutable"].append(name)
-else:
-    sorted_variables["immutable"].append(name)
-if isinstance(my_favourite_films,(list,dict,set)):
-    sorted_variables["mutable"].append(my_favourite_films)
-else:
-    sorted_variables["immutable"].append(my_favourite_films)
-if isinstance(profile_info,(list,dict,set)):
-    sorted_variables["mutable"].append(profile_info)
-else:
-    sorted_variables["immutable"].append(profile_info)
-if isinstance(marks,(list,dict,set)):
-    sorted_variables["mutable"].append(marks)
-else:
-    sorted_variables["immutable"].append(marks)
-if isinstance(collection_of_coins,(list,dict,set)):
-    sorted_variables["mutable"].append(collection_of_coins)
-else:
-    sorted_variables["immutable"].append(collection_of_coins)
-
+sorted_variables = {"mutable": [], "immutable":[]}
+variables =["lucky_number", "pi", "one_is_a_prime_number", "name", "my_favourite_films", "profile_info", "marks", "collection_of_coins"]
+for var in variables:
+    value = globals()[var]
+    if isinstance(value, (list, dict, set)):
+        sorted_variables["mutable"].append(var)
+    else:
+        sorted_variables["immutable"].append(var)
 print(sorted_variables)
-
