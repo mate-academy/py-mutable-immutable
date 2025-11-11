84d73f2119857526e5a35d9b71192e08ebb10cb5
Tanya210300 <gonchartanya9@gmail.com>
2025-11-11 14:39:02 +0200

    corrected

--- app/main.py
+++ app/main.py
@@ -16,38 +16,38 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-sorted_values = {"mutable":[], "immutable":[]}
+sorted_variables = {"mutable":[], "immutable":[]}
 if isinstance(lucky_number,(list,dict,set)):
-    sorted_values["mutable"].append("lucky_number")
+    sorted_variables["mutable"].append(lucky_number)
 else:
-    sorted_values["immutable"].append("lucky_number")
+    sorted_variables["immutable"].append(lucky_number)
 if isinstance(pi,(list,dict,set)):
-    sorted_values["mutable"].append("pi")
+    sorted_variables["mutable"].append(pi)
 else:
-    sorted_values["immutable"].append("pi")   
+    sorted_variables["immutable"].append(pi)   
 if isinstance(one_is_a_prime_number,(list,dict,set)):
-    sorted_values["mutable"].append("one_is_a_prime_number")
+    sorted_variables["mutable"].append(one_is_a_prime_number)
 else:
-    sorted_values["immutable"].append("one_is_a_prime_number")
+    sorted_variables["immutable"].append(one_is_a_prime_number)
 if isinstance(name,(list,dict,set)):
-    sorted_values["mutable"].append("name")
+    sorted_variables["mutable"].append(name)
 else:
-    sorted_values["immutable"].append("name")
+    sorted_variables["immutable"].append(name)
 if isinstance(my_favourite_films,(list,dict,set)):
-    sorted_values["mutable"].append("my_favourite_films")
+    sorted_variables["mutable"].append(my_favourite_films)
 else:
-    sorted_values["immutable"].append("my_favourite_films")
+    sorted_variables["immutable"].append(my_favourite_films)
 if isinstance(profile_info,(list,dict,set)):
-    sorted_values["mutable"].append("profile_info")
+    sorted_variables["mutable"].append(profile_info)
 else:
-    sorted_values["immutable"].append("profile_info")
+    sorted_variables["immutable"].append(profile_info)
 if isinstance(marks,(list,dict,set)):
-    sorted_values["mutable"].append("marks")
+    sorted_variables["mutable"].append(marks)
 else:
-    sorted_values["immutable"].append("marks")
+    sorted_variables["immutable"].append(marks)
 if isinstance(collection_of_coins,(list,dict,set)):
-    sorted_values["mutable"].append("collection_of_coins")
+    sorted_variables["mutable"].append(collection_of_coins)
 else:
-    sorted_values["immutable"].append("collection_of_coins")
-print(sorted_values)
+    sorted_variables["immutable"].append(collection_of_coins)
+print(sorted_variables)
 # test run
