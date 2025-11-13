3ec38edf1ee3b1cfca226d5856e331a705ba97ca
Jakub <jakubczupajlo@gmail.com>
2025-11-13 18:15:29 +0100

    feat: Final solution for mutable/immutable

--- app/main.py
+++ app/main.py
@@ -16,4 +16,8 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# Słownik z podziałem zmiennych na mutowalne i niemutowalne
+sorted_variables = {
+    "mutable": [my_favourite_films, marks, collection_of_coins],
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
+}
