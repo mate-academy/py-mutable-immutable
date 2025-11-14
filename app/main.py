903158508dbde3c543e31cb716fff2c90bf0e190
vladiukdaria <vladiukdaria@gmail.com>
2025-11-14 22:51:28 +0100

    Update main.py і

--- app/main.py
+++ app/main.py
@@ -17,6 +17,6 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [my_favourite_films],
+    "mutable": [my_favourite_films, collection_of_coins],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info]
 }
