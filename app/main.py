e721a47e4fa9eaf56e389c29de4a5683a0c6bd17
Tanya210300 <gonchartanya9@gmail.com>
2025-11-16 15:19:43 +0200

    CORRECT7

--- app/main.py
+++ app/main.py
@@ -16,10 +16,14 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 sorted_variables = {"mutable": [], "immutable":[]}
-variables ={'lucky_number': lucky_number, 'pi': pi, 'one_is_a_prime_number': one_is_a_prime_number, 'name': name,' my_favourite_films': my_favourite_films, 'profile_info': profile_info, 'marks': marks, 'collection_of_coins': collection_of_coins}
+variables ={'lucky_number': lucky_number, 'pi': pi, 'one_is_a_prime_number': one_is_a_prime_number, 'name': name,'my_favourite_films': my_favourite_films, 'profile_info': profile_info, 'marks': marks, 'collection_of_coins': collection_of_coins}
 immutable_types = (int, float, bool, str, tuple, frozenset, bytes, type(None))
 for name, value in variables.items():
     if isinstance(value, immutable_types):
         sorted_variables["immutable"].append(name)
     else:
         sorted_variables["mutable"].append(name)
+print("sorted_variables = {")
+print(" mutable: [" + ", ".join(sorted_variables["mutable"]) + "],")
+print(" immutable: [" + ", ".join(sorted_variables["immutable"]) + "]")
+print("}")
