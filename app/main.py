64c12c7f5c9f209a4e1dfab7a51381751af062b2
Luciano Herval <lucianoherval@gmail.com>
2025-11-22 15:56:10 -0300

    Updatte Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
-lucky_number = 777 # immutable
-pi = 3.14 # immutable
-one_is_a_prime_number = False # immutable
-name = "Richard" # immutable
+lucky_number = 777
+
+pi = 3.14
+
+one_is_a_prime_number = False
+
+name = "Richard"
+
 my_favourite_films = [
     "The Shawshank Redemption",
     "The Lord of the Rings: The Return of the King",
     "Pulp Fiction",
     "The Good, the Bad and the Ugly",
     "The Matrix",
-] #mutavel
+]
+
 profile_info = ("michel", "michel@gmail.com", "12345678")
 marks = {
     "John": 4,
     "Sergio": 3,
 }
+
 collection_of_coins = {1, 2, 25}
 
 mutable_list = [my_favourite_films, marks, collection_of_coins]
