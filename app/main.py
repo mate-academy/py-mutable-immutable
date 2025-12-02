180692dcd99647869f2084cfac3bd5263976e609
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-02 18:37:02 -0500

    Removed trailing spaces

--- app/main.py
+++ app/main.py
@@ -19,6 +19,6 @@ collection_of_coins = {1, 2, 25}
 sorted_variables = {
     "mutable": [my_favourite_films, collection_of_coins, my_favourite_films,
                 marks],
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, 
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name,
                   profile_info]
 }
