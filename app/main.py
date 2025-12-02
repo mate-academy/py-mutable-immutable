13c0b7a5a1530c8ce6e440e47ffe7fb595e6e80d
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-02 18:40:16 -0500

    Remove doublons

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ marks = {
 collection_of_coins = {1, 2, 25}
 
 sorted_variables = {
-    "mutable": [my_favourite_films, collection_of_coins, my_favourite_films,
-                marks],
+    "mutable": [my_favourite_films, collection_of_coins, marks],
     "immutable": [lucky_number, pi, one_is_a_prime_number, name,
                   profile_info]
 }
