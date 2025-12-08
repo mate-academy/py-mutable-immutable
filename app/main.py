199d7b672892364bf3d8c402bf79023e7390e9a7
samuel <samuel.macias.f@gmail.com>
2025-12-08 12:09:10 -0500

    resolved problems

--- app/main.py
+++ app/main.py
@@ -1,19 +1,27 @@
-lucky_number = 777
+# Variables
+lucky_number = 7
 pi = 3.14
 one_is_a_prime_number = False
-name = "Richard"
-my_favourite_films = [
-    "The Shawshank Redemption",
-    "The Lord of the Rings: The Return of the King",
-    "Pulp Fiction",
-    "The Good, the Bad and the Ugly",
-    "The Matrix",
-]
-profile_info = ("michel", "michel@gmail.com", "12345678")
-marks = {
-    "John": 4,
-    "Sergio": 3,
-}
-collection_of_coins = {1, 2, 25}
+name = "Samuel"
 
-# write your code here
+my_favourite_films = ["Interstellar", "Inception", "The Matrix"]
+profile_info = {"age": 18, "country": "Colombia"}
+marks = (5, 4, 5)
+collection_of_coins = {50, 100, 200}
+
+# Lista correcta según los tests
+sorted_variables = [
+    lucky_number,
+    pi,
+    one_is_a_prime_number,
+    name,
+    my_favourite_films,
+    profile_info,
+    marks,
+    collection_of_coins,
+]
+
+
+def count_occurrences(phrase: str, letter: str) -> int:
+    return phrase.lower().count(letter.lower())
+
